f78b9e0bba3b3c9a5c149c9a005720fa21bbbc38
Petr Vorel <pvorel@suse.cz>
2019-11-01 14:48:16 +0100

    net/route: Add netlink based route change gw/dest tests
    * use libmnl and new C API
    * reuse code in tst_net.h (added in previous commit)
    * add shell wrappers (set environment with tst_net.sh instead of using
      deprecated helpers in testcases/network/stress/ns-tools/)
    
    Travis:
    * add libmnl libraries to most of travis jobs
    
    Cleanup:
    * cleanup test description
    * other cleanup
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/network/stress/route/route-lib.sh
+++ testcases/network/stress/route/route-lib.sh
@@ -6,9 +6,41 @@ TST_NEEDS_ROOT=1
 TST_SETUP="setup"
 TST_CLEANUP="route_cleanup"
 TST_NEEDS_CMDS="ip"
-TST_CNT=$NS_TIMES
 
 . tst_net.sh
+
+MAX_IP=${MAX_IP:-5}
+
+check_max_ip()
+{
+	local max_ip_limit=254
+	[ "$TST_IPV6" ] && max_ip_limit=65534
+
+	tst_is_int $MAX_IP || tst_brk TBROK "\$MAX_IP not int ($MAX_IP)"
+	[ "$MAX_IP" -gt $max_ip_limit ] && MAX_IP=$max_ip_limit
+}
+
+setup_gw()
+{
+	tst_res TINFO "change IPv$TST_IPVER route gateway $NS_TIMES times"
+
+	rt="$(tst_ipaddr_un -p 0 0)"
+	lhost="$(tst_ipaddr_un 1 1)"
+	rhost="$(tst_ipaddr_un 0 1)"
+	tst_add_ipaddr -s -q -a $lhost
+	tst_add_ipaddr -s -q -a $rhost rhost
+}
+
+
+test_netlink()
+{
+	local ip_flag
+	[ "$TST_IPV6" ] && ip_flag="-6"
+
+	local port=$(tst_rhost_run -s -c "tst_get_unused_port ipv${TST_IPVER} dgram")
+
+	EXPECT_PASS route-change-netlink -c $NS_TIMES -d $(tst_iface) $ip_flag -p $port $ROUTE_CHANGE_NETLINK_PARAMS
+}
 
 route_cleanup()
 {
