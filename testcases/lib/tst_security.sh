c7c4cd5e7f3b4db40b4bd09ff2b476a5ab66b391
Lakshmi Ramasubramanian <nramas@linux.microsoft.com>
2020-12-18 18:40:08 +0100

    tst_security.sh: Add SELinux helpers
    reusable in tests:
    
    tst_require_selinux_enabled and tst_update_selinux_state
    
    Signed-off-by: Lakshmi Ramasubramanian <nramas@linux.microsoft.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/lib/tst_security.sh
+++ testcases/lib/tst_security.sh
@@ -140,3 +140,19 @@ tst_get_enforce()
 	local f="$dir/enforce"
 	[ -f "$f" ] && echo "$f"
 }
+
+tst_require_selinux_enabled()
+{
+	tst_selinux_enabled || tst_brk TCONF "SELinux not enabled"
+}
+
+tst_update_selinux_state()
+{
+	local cur_val new_val
+	local dir=$(tst_get_selinux_dir)
+	[ -z "$dir" ] || return 1
+
+	cur_val=$(cat $dir/checkreqprot)
+	[ $cur_val = 1 ] && new_val=0 || new_val=1
+	echo $new_val > $dir/checkreqprot
+}
