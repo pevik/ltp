pevik/ltp

commit 8a0827766db8d37ecf717137864a0561f6a40f5b

lib: add functions to adjust oom score
This introduces function to LTP to adjust the oom_score_adj of target
process, which may be helpful in OOM tests to prevent kernel killing
the main or lib process during test run.

The exported global tst_enable_oom_protection function can be used
at anywhere you want to protect, but please remember that if you
do enable protection on a process($PID) that all the children will
inherit its score and be ignored by OOM Killer as well. So that's
why tst_disable_oom_protection to be used in combination.

Signed-off-by: Li Wang <liwang@redhat.com>
Reviewed-by: Petr Vorel <pvorel@suse.cz>
Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

include/tst_memutils.h
@@ -25,4 +25,24 @@ void tst_pollute_memory(size_t maxsize, int fillchar);
  */
 long long tst_available_mem(void);
 
+/*
+ * Enable OOM protection to prevent process($PID) being killed by OOM Killer.
+ *   echo -1000 >/proc/$PID/oom_score_adj
+ * If the pid is 0 which means it will set on current(self) process.
+ *
+ * Note:
+ *  This exported tst_enable_oom_protection function can be used at anywhere
+ *  you want to protect, but please remember that if you do enable protection
+ *  on a process($PID) that all the children will inherit its score and be
+ *  ignored by OOM Killer as well. So that's why tst_disable_oom_protection
+ *  to be used in combination.
+ */
+void tst_enable_oom_protection(pid_t pid);
+
+/*
+ * Disable the OOM protection for the process($PID).
+ *   echo 0 >/proc/$PID/oom_score_adj
+ */
+void tst_disable_oom_protection(pid_t pid);
+
 #endif /* TST_MEMUTILS_H__ */

lib/tst_memutils.c
@@ -3,6 +3,7 @@
  * Copyright (c) 2020 SUSE LLC <mdoucha@suse.cz>
  */
 
+#include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
 #include <sys/sysinfo.h>
@@ -90,4 +91,45 @@ long long tst_available_mem(void)
 	}
 
 	return mem_available;
+}
+
+static void set_oom_score_adj(pid_t pid, int value)
+{
+	int val;
+	char score_path[64];
+
+	if (access("/proc/self/oom_score_adj", F_OK) == -1) {
+		tst_res(TINFO, "oom_score_adj does not exist, skipping the adjustement");
+		return;
+	}
+
+	if (pid == 0) {
+		sprintf(score_path, "/proc/self/oom_score_adj");
+	} else {
+		sprintf(score_path, "/proc/%d/oom_score_adj", pid);
+		if (access(score_path, F_OK) == -1)
+			tst_brk(TBROK, "%s does not exist, please check if PID is valid", score_path);
+	}
+
+	FILE_PRINTF(score_path, "%d", value);
+	FILE_SCANF(score_path, "%d", &val);
+
+	if (val != value) {
+		if (value < 0) {
+			tst_res(TWARN, "'%s' cannot be set to %i, are you root?",
+				score_path, value);
+			return;
+		}
+		tst_brk(TBROK, "oom_score_adj = %d, but expect %d.", val, value);
+	}
+}
+
+void tst_enable_oom_protection(pid_t pid)
+{
+	set_oom_score_adj(pid, -1000);
+}
+
+void tst_disable_oom_protection(pid_t pid)
+{
+	set_oom_score_adj(pid, 0);
 }

commit c1d0cae3c0bce59b33d2aebc3c5a75364a88a9db

lib: Skip tst_{disable,enable}_oom_protection() for non-root
If needed to set value also for non-root, use set_oom_score_adj().

Fixes: 8a0827766d ("lib: add functions to adjust oom score")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/tst_memutils.h
@@ -30,11 +30,15 @@ long long tst_available_mem(void);
  *   echo -1000 >/proc/$PID/oom_score_adj
  * If the pid is 0 which means it will set on current(self) process.
  *
+ * WARNING:
+ *  Do nothing for non-root, because setting value < 0 requires root.
+    If you want to set value also for non-root, use set_oom_score_adj().
+ *
  * Note:
  *  This exported tst_enable_oom_protection function can be used at anywhere
  *  you want to protect, but please remember that if you do enable protection
  *  on a process($PID) that all the children will inherit its score and be
- *  ignored by OOM Killer as well. So that's why tst_disable_oom_protection
+ *  ignored by OOM Killer as well. So that's why tst_disable_oom_protection()
  *  to be used in combination.
  */
 void tst_enable_oom_protection(pid_t pid);
@@ -42,6 +46,11 @@ void tst_enable_oom_protection(pid_t pid);
 /*
  * Disable the OOM protection for the process($PID).
  *   echo 0 >/proc/$PID/oom_score_adj
+ *
+ * WARNING:
+ *  Do nothing for non-root, because it's expected to be cleanup after
+ *  tst_enable_oom_protection(). Use set_oom_score_adj(), if you want to set
+ *  value also for non-root.
  */
 void tst_disable_oom_protection(pid_t pid);
 

lib/tst_memutils.c
@@ -8,9 +8,12 @@
 #include <limits.h>
 #include <sys/sysinfo.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define TST_NO_DEFAULT_MAIN
 #include "tst_test.h"
+#include "tst_capability.h"
+#include "lapi/syscalls.h"
 
 #define BLOCKSIZE (16 * 1024 * 1024)
 
@@ -124,12 +127,41 @@ static void set_oom_score_adj(pid_t pid, int value)
 	}
 }
 
+static bool check_caps(void)
+{
+	struct tst_cap_user_header hdr = {
+		.version = 0x20080522,
+		.pid = tst_syscall(__NR_gettid),
+	};
+
+	struct tst_cap_user_data caps[2] = { {0} };
+
+	if (tst_capget(&hdr, caps))
+		tst_brk(TBROK | TERRNO, "tst_capget()");
+
+	tst_res(TINFO, "pev: CAP_SYS_ADMIN: %d", caps[0].effective & (1U << CAP_SYS_ADMIN));
+	tst_res(TINFO, "pev: CAP_SYS_RESOURCE: %d", caps[0].effective & (1U << CAP_SYS_RESOURCE));
+	if (!(caps[0].effective & (1U << CAP_SYS_ADMIN)) ||
+	    !(caps[0].effective & (1U << CAP_SYS_RESOURCE)))
+		return false;
+
+	return true;
+}
+
 void tst_enable_oom_protection(pid_t pid)
 {
+	fprintf(stderr, "%s:%d %s(): call: check_caps\n", __FILE__, __LINE__, __func__); // FIXME: debug
+	if (!check_caps())
+		return;
+
 	set_oom_score_adj(pid, -1000);
 }
 
 void tst_disable_oom_protection(pid_t pid)
 {
+	fprintf(stderr, "%s:%d %s(): call: check_caps\n", __FILE__, __LINE__, __func__); // FIXME: debug
+	if (!check_caps())
+		return;
+
 	set_oom_score_adj(pid, 0);
 }