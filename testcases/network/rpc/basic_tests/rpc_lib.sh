26929f6c0a4c7bc4ed6247fdd084c504c4af2d8d
Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
2022-01-20 17:37:28 +0300

    rpc_lib.sh: fix portmapper detection in case of socket activation
    On systemd-based linux hosts, rpcbind service is typically started via
    socket activation, when the first client connects. If no client has
    connected before LTP rpc test starts, rpcbind process will not be
    running at the time of check_portmap_rpcbind() execution, causing
    check_portmap_rpcbind() to report TCONF error.
    
    Fix that by adding a quiet invocation of 'rpcinfo' before checking for
    rpcbind.
    
    For portmap, similar step is likely not needed, because portmap is used
    only on old systemd and those don't use systemd.
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>

--- testcases/network/rpc/basic_tests/rpc_lib.sh
+++ testcases/network/rpc/basic_tests/rpc_lib.sh
@@ -8,6 +8,12 @@ check_portmap_rpcbind()
 	if pgrep portmap > /dev/null; then
 		PORTMAPPER="portmap"
 	else
+		# In case of systemd socket activation, rpcbind could be
+		# not started until somebody tries to connect to it's socket.
+		#
+		# To handle that case properly, run a client now.
+		rpcinfo >/dev/null 2>&1
+
 		pgrep rpcbind > /dev/null && PORTMAPPER="rpcbind" || \
 			tst_brk TCONF "portmap or rpcbind is not running"
 	fi
