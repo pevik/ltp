c188c926bff3f878fc0d9f2f8f945f28b3f0cbb0
Petr Vorel <pvorel@suse.cz>
2022-01-26 12:07:08 +0100

    lib/cloner.c: Remove ia64 and hppa workarounds
    ia64 is EOL since 2021 and thus orphaned in v5.11
    (commit 228345bf98cd). We already removed some ia64 fixes.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- lib/cloner.c
+++ lib/cloner.c
@@ -33,62 +33,10 @@
 
 #undef clone			/* we want to use clone() */
 
-/*
- * The ia64 port has never included a prototype for __clone2(). It was updated
- * to take eight parameters in glibc commit:
- *
- * commit 625f22fc7f8e0d61e3e6cff2c65468b91dbad426
- * Author: Ulrich Drepper <drepper@redhat.com>
- * Date:   Mon Mar 3 19:53:27 2003 +0000
- *
- * The first release that contained this commit was glibc-2.3.3 which is old
- * enough to assume that __clone2() takes eight parameters.
- */
-#if defined(__ia64__)
-extern int __clone2(int (*fn) (void *arg), void *child_stack_base,
-                    size_t child_stack_size, int flags, void *arg,
-                    pid_t *parent_tid, void *tls, pid_t *child_tid);
-#endif
-
-/*
- * ltp_clone: wrapper for clone to hide the architecture dependencies.
- *   1. hppa takes bottom of stack and no stacksize (stack grows up)
- *   2. __ia64__ takes bottom of stack and uses clone2
- *   3. all others take top of stack (stack grows down)
- */
-static int
-ltp_clone_(unsigned long flags, int (*fn)(void *arg), void *arg,
-	   size_t stack_size, void *stack, pid_t *ptid, void *tls, pid_t *ctid)
-{
-	int ret;
-
-#if defined(__ia64__)
-	ret = __clone2(fn, stack, stack_size, flags, arg, ptid, tls, ctid);
-#else
-# if defined(__hppa__) || defined(__metag__)
-	/*
-	 * These arches grow their stack up, so don't need to adjust the base.
-	 * XXX: This should be made into a runtime test.
-	 */
-# else
-	/*
-	 * For archs where stack grows downwards, stack points to the topmost
-	 * address of the memory space set up for the child stack.
-	 */
-	if (stack)
-		stack += stack_size;
-# endif
-
-	ret = clone(fn, stack, flags, arg, ptid, tls, ctid);
-#endif
-
-	return ret;
-}
-
 int ltp_clone(unsigned long flags, int (*fn)(void *arg), void *arg,
               size_t stack_size, void *stack)
 {
-	return ltp_clone_(flags, fn, arg, stack_size, stack, NULL, NULL, NULL);
+	return clone(fn, stack, flags, arg);
 }
 
 int ltp_clone7(unsigned long flags, int (*fn)(void *arg), void *arg,
@@ -104,7 +52,7 @@ int ltp_clone7(unsigned long flags, int (*fn)(void *arg), void *arg,
 	ctid = va_arg(arg_clone, pid_t *);
 	va_end(arg_clone);
 
-	return ltp_clone_(flags, fn, arg, stack_size, stack, ptid, tls, ctid);
+	return clone(fn, stack, flags, arg, ptid, tls, ctid);
 }
 
 /*
