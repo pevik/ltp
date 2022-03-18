b54f9ba947298e0a7a13b53785ae37094f8f6b10
Dai Shili <daisl.fnst@fujitsu.com>
2022-03-18 14:55:47 -0400

    syscalls/umount2: Convert to new API and use SAFE_ACCESS
    1. use TST_EXP_FAIL and TST_EXP_PASS macro
    2. use SAFE macro
    3. simplify verify operations
    4. merge umount2_03 to umount2_02
    
    Signed-off-by: Dai Shili <daisl.fnst@fujitsu.com>

--- testcases/kernel/syscalls/umount2/umount2.h
+++ testcases/kernel/syscalls/umount2/umount2.h
@@ -1,18 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2016 Cyril Hrubis <chrubis@suse.cz>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef UMOUNT2_H__
@@ -28,13 +16,13 @@ static inline int umount2_retry(const char *target, int flags)
 		if (ret == 0 || errno != EBUSY)
 			return ret;
 
-		tst_resm(TINFO, "umount('%s', %i) failed with EBUSY, try %2i...",
+		tst_res(TINFO, "umount('%s', %i) failed with EBUSY, try %2i...",
 			 target, flags, i);
 
 		usleep(100000);
 	}
 
-	tst_resm(TWARN, "Failed to umount('%s', %i) after 50 retries",
+	tst_res(TWARN, "Failed to umount('%s', %i) after 50 retries",
 	         target, flags);
 
 	errno = EBUSY;
