7218865de6952dd50b28a137bd787783e16a8e2e
Andrea Cervesato <andrea.cervesato@suse.com>
2022-08-08 16:16:32 +0200

    Refactor pidns17 test using new LTP API
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/containers/pidns/pidns17.c
+++ testcases/kernel/containers/pidns/pidns17.c
@@ -1,162 +1,75 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
-* Copyright (c) International Business Machines Corp., 2007
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-* the GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-*
-***************************************************************************
-* File: pidns17.c
-* *
-* * Description:
-* *  The pidns17.c testcase verifies inside the container, if kill(-1, SIGUSR1)
-* *  terminates all children running inside.
-* *
-* * Test Assertion & Strategy:
-* *  Create a PID namespace container.
-* *  Spawn many children inside it.
-* *  Invoke kill(-1, SIGUSR1) inside container and check if it terminates
-* *  all children.
-* *
-* * Usage: <for command-line>
-* *  pidns17
-* *
-* * History:
-* *  DATE      NAME                             DESCRIPTION
-* *  13/11/08  Gowrishankar M 			Creation of this test.
-* *            <gowrishankar.m@in.ibm.com>
-*
-******************************************************************************/
-#define _GNU_SOURCE 1
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
-#include "pidns_helper.h"
-#include "test.h"
-
-char *TCID = "pidns17";
-int TST_TOTAL = 1;
-
-int child_fn(void *);
-
-#define CHILD_PID       1
-#define PARENT_PID      0
-
-/*
- * child_fn() - Inside container
+ * Copyright (c) International Business Machines Corp., 2007
+ *               13/11/08  Gowrishankar M <gowrishankar.m@in.ibm.com>
+ * Copyright (C) 2022 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
-int child_fn(void *arg)
-{
-	int children[10], exit_val, i, status;
-	pid_t pid, ppid;
 
-	/* Set process id and parent pid */
-	pid = getpid();
+/*\
+ * [Description]
+ *
+ * Clone a process with CLONE_NEWPID flag and spawn many children inside the
+ * container. Then terminate all children and check if they ended up.
+ */
+
+#include <sys/wait.h>
+#include "tst_test.h"
+#include "lapi/namespaces_constants.h"
+
+#define CHILDREN_NUM 10
+
+static int child_func(LTP_ATTRIBUTE_UNUSED void *arg)
+{
+	int children[CHILDREN_NUM], status;
+	unsigned int i, counter = 0;
+	pid_t cpid, ppid;
+
+	cpid = getpid();
 	ppid = getppid();
-	if (pid != CHILD_PID || ppid != PARENT_PID) {
-		printf("cinit: pidns was not created\n");
-		exit(1);
+
+	if (cpid != 1 || ppid != 0) {
+		tst_res(TFAIL, "Got unexpected result of cpid=%d ppid=%d", cpid, ppid);
+		return 1;
 	}
 
-	exit_val = 0;
-
-	/* Spawn many children */
-	for (i = 0; i < ARRAY_SIZE(children); i++) {
-		switch ((children[i] = fork())) {
-		case -1:
-			perror("fork failed");
-			exit_val = 1;
-			break;
-		case 0:
+	for (i = 0; i < CHILDREN_NUM; i++) {
+		children[i] = SAFE_FORK();
+		if (!children[i]) {
 			pause();
-			/* XXX (garrcoop): why exit with an exit code of 2? */
-			exit(2);
-			break;
-		default:
-			/* fork succeeded. */
-			break;
+			return 0;
 		}
 	}
-	/* wait for last child to get scheduled */
-	sleep(1);
 
-	if (kill(-1, SIGUSR1) == -1) {
-		perror("cinit: kill(-1, SIGUSR1) failed");
-		exit_val = 1;
+	SAFE_KILL(-1, SIGUSR1);
+
+	for (i = 0; i < CHILDREN_NUM; i++) {
+		SAFE_WAITPID(children[i], &status, 0);
+
+		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGUSR1)
+			counter++;
 	}
 
-	for (i = 0; i < ARRAY_SIZE(children); i++) {
-		if (waitpid(children[i], &status, 0) == -1) {
-			perror("cinit: waitpid failed");
-			kill(children[i], SIGTERM);
-			waitpid(children[i], &status, 0);
-			exit_val = 1;
-		}
-		if (!(WIFSIGNALED(status) || WTERMSIG(status) == SIGUSR1)) {
-			/*
-			 * XXX (garrcoop): this status reporting is overly
-			 * noisy. Someone obviously needs to read the
-			 * constraints documented in wait(2) a bit more
-			 * closely -- in particular the relationship between
-			 * WIFEXITED and WEXITSTATUS, and WIFSIGNALED and
-			 * WTERMSIG.
-			 */
-			printf("cinit: found a child alive still "
-			       "%d exit: %d, %d, signal %d, %d", i,
-			       WIFEXITED(status), WEXITSTATUS(status),
-			       WIFSIGNALED(status), WTERMSIG(status));
-			exit_val = 1;
-		}
+	if (counter != CHILDREN_NUM) {
+		tst_res(TFAIL, "%d children didn't end", CHILDREN_NUM - counter);
+		return 1;
 	}
-	if (exit_val == 0)
-		printf("cinit: all children have terminated.\n");
 
-	exit(exit_val);
+	tst_res(TPASS, "All children ended successfuly");
+
+	return 0;
 }
 
-static void setup(void)
+static void run(void)
 {
-	tst_require_root();
-	check_newpid();
+	int ret;
+
+	ret = ltp_clone_quick(CLONE_NEWPID | SIGCHLD, child_func, NULL);
+	if (ret < 0)
+		tst_brk(TBROK | TERRNO, "clone failed");
 }
 
-int main(void)
-{
-	int status;
-	pid_t pid;
-
-	setup();
-
-	pid = getpid();
-
-	/* Container creation on PID namespace */
-	TEST(do_clone_unshare_test(T_CLONE, CLONE_NEWPID, child_fn, NULL));
-	if (TEST_RETURN == -1) {
-		tst_brkm(TBROK | TTERRNO, NULL, "clone failed");
-	}
-
-	sleep(1);
-	if (wait(&status) == -1)
-		tst_resm(TFAIL, "waitpid failed");
-
-	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-		tst_resm(TFAIL, "container exited abnormally");
-	else if (WIFSIGNALED(status))
-		tst_resm(TFAIL,
-			 "container was signaled with signal = %d",
-			 WTERMSIG(status));
-
-	tst_exit();
-
-}
+static struct tst_test test = {
+	.test_all = run,
+	.needs_root = 1,
+	.forks_child = 1,
+};
