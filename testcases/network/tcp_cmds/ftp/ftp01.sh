894ef715dab6790dc2c0e4f2c44cf68aa00342f3
Akihiko Odaki <akihiko.odaki@daynix.com>
2022-10-20 21:07:41 +0900

    ftp/ftp01: Split the test function
    Suggested-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- testcases/network/tcp_cmds/ftp/ftp01.sh
+++ testcases/network/tcp_cmds/ftp/ftp01.sh
@@ -49,56 +49,66 @@ do_setup()
 {
 
     TC=ftp
-    ASCII_FILES=${ASCII_FILES:-"ascii.sm ascii.med ascii.lg ascii.jmb"}
-    BIN_FILES=${BIN_FILES:-"bin.sm bin.med bin.lg bin.jmb"}
-
     RUSER=${RUSER:-root}
 
 }
 
 do_test()
 {
+    case $1 in
+    1) test_get binary;;
+    2) test_get ascii;;
+    3) test_put binary;;
+    4) test_put ascii;;
+    esac
+}
+
+list_files()
+{
+    case $1 in
+    binary) echo 'ascii.sm ascii.med ascii.lg ascii.jmb';;
+    ascii) echo 'bin.sm bin.med bin.lg bin.jmb';;
+    esac
+}
+
+test_get()
+{
     local sum1 sum2
 
-    for i in binary ascii; do
+    for file in $(list_files $1); do
+        {
+            echo user $RUSER $PASSWD
+            echo $1
+            echo cd $TST_NET_DATAROOT
+            echo get $file
+            echo quit
+        } | ftp -nv $RHOST
 
-        if [ $i = "binary" ]; then
-            FILES=$BIN_FILES
-        fi
-        if [ $i = "ascii" ]; then
-            FILES=$ASCII_FILES
-        fi
-        for j in $FILES; do
+        sum1="$(ls -l $file  | awk '{print $5}')"
+        sum2="$(ls -l $TST_NET_DATAROOT/$file | awk '{print $5}')"
+        rm -f $file
+        EXPECT_PASS "[ '$sum1' = '$sum2' ]"
+    done
+}
 
-            for a in get put; do
-                if [ $a = "get" ]; then
-                    {
-                        echo user $RUSER $PASSWD
-                        echo $i
-                        echo cd $TST_NET_DATAROOT
-                        echo $a $j
-                        echo quit
-                    } | ftp -nv $RHOST
-                    sum1="$(ls -l $j  | awk '{print $5}')"
-                    sum2="$(ls -l $TST_NET_DATAROOT/$j | awk '{print $5}')"
-                    rm -f $j
-                else
-                    {
-                        echo user $RUSER $PASSWD
-                        echo lcd $TST_NET_DATAROOT
-                        echo $i
-                        echo cd $TST_TMPDIR
-                        echo $a $j
-                        echo quit
-                    } | ftp -nv $RHOST
-                    sum1="$(tst_rhost_run -c "sum $TST_TMPDIR/$j" -s | awk '{print $1}')"
-                    sum2="$(sum $TST_NET_DATAROOT/$j | awk '{print $1}')"
-                    tst_rhost_run -c "rm -f $TST_TMPDIR/$j"
-                fi
+test_put()
+{
+    local sum1 sum2
 
-                EXPECT_PASS "[ '$sum1' = '$sum2' ]"
-            done
-        done
+    for file in $(list_files $1); do
+        {
+            echo user $RUSER $PASSWD
+            echo lcd $TST_NET_DATAROOT
+            echo $1
+            echo cd $TST_TMPDIR
+            echo post $file
+            echo quit
+        } | ftp -nv $RHOST
+
+        sum1="$(tst_rhost_run -c "sum $TST_TMPDIR/$file" -s | awk '{print $1}')"
+        sum2="$(sum $TST_NET_DATAROOT/$file | awk '{print $1}')"
+        tst_rhost_run -c "rm -f $TST_TMPDIR/$file"
+        EXPECT_PASS "[ '$sum1' = '$sum2' ]"
     done
 }
 
