aeff45249ef020a8df0725b3979a5ccc4f887289
Andrea Cervesato <andrea.cervesato@suse.com>
2022-10-31 12:30:46 +0100

    Rewrite utsname testing suite
    Deleted utstest.c and created the following new tests:
    - utsname01
    - utsname02
    - utsname03
    - utsname04
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

---
+++ testcases/kernel/containers/utsname/utsname.h
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2022 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
+ */
+
+#ifndef UTSTEST_H
+#define UTSTEST_H
+
+#include <stdlib.h>
+#include "tst_test.h"
+#include "lapi/syscalls.h"
+#include "lapi/namespaces_constants.h"
+
+enum {
+	T_CLONE,
+	T_UNSHARE,
+	T_NONE,
+};
+
+static int dummy_child(LTP_ATTRIBUTE_UNUSED void *v)
+{
+	return 0;
+}
+
+static inline void check_newuts(void)
+{
+	int pid, status;
+
+	pid = ltp_clone_quick(CLONE_NEWUTS | SIGCHLD, dummy_child, NULL);
+	if (pid < 0)
+		tst_brk(TCONF | TERRNO, "CLONE_NEWIPC not supported");
+
+	SAFE_WAITPID(pid, &status, 0);
+}
+
+static inline int get_clone_unshare_enum(const char *str_op)
+{
+	int use_clone;
+
+	use_clone = T_NONE;
+
+	if (!str_op || !strcmp(str_op, "none"))
+		use_clone = T_NONE;
+	else if (!strcmp(str_op, "clone"))
+		use_clone = T_CLONE;
+	else if (!strcmp(str_op, "unshare"))
+		use_clone = T_UNSHARE;
+	else
+		tst_brk(TBROK, "Test execution mode <clone|unshare|none>");
+
+	return use_clone;
+}
+
+static inline pid_t clone_test(unsigned long clone_flags, int (*fn1)(void *arg), void *arg1)
+{
+	pid_t pid;
+
+	pid = ltp_clone_quick(clone_flags | SIGCHLD, fn1, arg1);
+	if (pid < 0)
+		tst_brk(TBROK | TERRNO, "ltp_clone_quick error");
+
+	return pid;
+}
+
+static inline pid_t unshare_test(unsigned long clone_flags, int (*fn1)(void *arg), void *arg1)
+{
+	pid_t pid;
+
+	pid = SAFE_FORK();
+	if (!pid) {
+		SAFE_UNSHARE(clone_flags);
+
+		fn1(arg1);
+		exit(0);
+	}
+
+	return pid;
+}
+
+static inline pid_t plain_test(int (*fn1)(void *arg), void *arg1)
+{
+	pid_t pid;
+
+	pid = SAFE_FORK();
+	if (!pid) {
+		fn1(arg1);
+		exit(0);
+	}
+
+	return pid;
+}
+
+static inline pid_t clone_unshare_test(int use_clone, unsigned long clone_flags,
+			       int (*fn1)(void *arg), void *arg1)
+{
+	pid_t pid = -1;
+
+	switch (use_clone) {
+	case T_NONE:
+		pid = plain_test(fn1, arg1);
+	break;
+	case T_CLONE:
+		pid = clone_test(clone_flags, fn1, arg1);
+	break;
+	case T_UNSHARE:
+		pid = unshare_test(clone_flags, fn1, arg1);
+	break;
+	default:
+		tst_brk(TBROK, "%s: bad use_clone option: %d", __func__, use_clone);
+	break;
+	}
+
+	return pid;
+}
+
+#endif
---
+++ testcases/kernel/containers/utsname/utsname02.c
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2022 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
+ */
+
+/*\
+ * [Description]
+ *
+ * Clone two plain processes, change hostname in the first one then check if
+ * hostaname has changed inside the second one as well.
+ */
+
+#define _GNU_SOURCE
+
+#include "tst_test.h"
+#include "utsname.h"
+
+#define HLEN 100
+#define HOSTNAME "LTP_HOSTNAME"
+
+static char *hostname1;
+static char *hostname2;
+static char originalhost[HLEN];
+
+static void reset_hostname(void)
+{
+	SAFE_SETHOSTNAME(originalhost, strlen(originalhost));
+}
+
+static int child1_run(LTP_ATTRIBUTE_UNUSED void *vtest)
+{
+	SAFE_SETHOSTNAME(HOSTNAME, strlen(HOSTNAME));
+	SAFE_GETHOSTNAME(hostname1, HLEN);
+
+	TST_CHECKPOINT_WAKE(0);
+
+	return 0;
+}
+
+static int child2_run(LTP_ATTRIBUTE_UNUSED void *vtest)
+{
+	TST_CHECKPOINT_WAIT(0);
+
+	SAFE_GETHOSTNAME(hostname2, HLEN);
+
+	return 0;
+}
+
+static void run(void)
+{
+	pid_t pid1, pid2;
+	int status1, status2;
+
+	memset(hostname1, 0, HLEN);
+	memset(hostname2, 0, HLEN);
+
+	pid1 = clone_unshare_test(T_NONE, 0, child1_run, NULL);
+	pid2 = clone_unshare_test(T_NONE, 0, child2_run, NULL);
+
+	SAFE_WAITPID(pid1, &status1, 0);
+	SAFE_WAITPID(pid2, &status2, 0);
+
+	if (WIFSIGNALED(status1) || WIFSIGNALED(status2))
+		return;
+
+	TST_EXP_PASS(strcmp(hostname1, HOSTNAME));
+	TST_EXP_PASS(strcmp(hostname2, HOSTNAME));
+
+	reset_hostname();
+}
+
+static void setup(void)
+{
+	hostname1 = SAFE_MMAP(NULL, sizeof(char) * HLEN, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	hostname2 = SAFE_MMAP(NULL, sizeof(char) * HLEN, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	memset(originalhost, 0, HLEN);
+
+	SAFE_GETHOSTNAME(originalhost, HLEN);
+}
+
+static void cleanup(void)
+{
+	SAFE_MUNMAP(hostname1, HLEN);
+	SAFE_MUNMAP(hostname2, HLEN);
+
+	reset_hostname();
+}
+
+static struct tst_test test = {
+	.test_all = run,
+	.setup = setup,
+	.cleanup = cleanup,
+	.needs_root = 1,
+	.forks_child = 1,
+	.needs_checkpoints = 1,
+};
