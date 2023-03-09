pevik/ltp

commit b857f8723f30a4b9554bf6b0ff8fa52fd07e8b60

lapi/fsmount: resolve conflict in different header files
The latest glibc added new wrappers (e.g. mount_setattr, fsopen) support
in sys/mount.h, which partly conflicts with linux/mount.h at the same time.

We need to make adjustments to header files to fix compiling error on
different platforms.

Signed-off-by: Li Wang <liwang@redhat.com>
Reviewed-by: Petr Vorel <pvorel@suse.cz>

include/lapi/fsmount.h
@@ -11,9 +11,12 @@
 #include "config.h"
 #include <sys/syscall.h>
 #include <sys/types.h>
+#include <sys/mount.h>
 
-#ifdef HAVE_LINUX_MOUNT_H
-# include <linux/mount.h>
+#ifndef HAVE_FSOPEN
+# ifdef HAVE_LINUX_MOUNT_H
+#  include <linux/mount.h>
+# endif
 #endif
 
 #include "lapi/fcntl.h"

configure.ac
@@ -113,6 +113,7 @@ AC_CHECK_FUNCS_ONCE([ \
     mkdirat \
     mknodat \
     modify_ldt \
+    mount_setattr \
     move_mount \
     name_to_handle_at \
     open_tree \

include/lapi/fs.h
@@ -6,8 +6,10 @@
  * Email: code@zilogic.com
  */
 
-#ifdef HAVE_LINUX_FS_H
-# include <linux/fs.h>
+#ifndef HAVE_MOUNT_SETATTR
+# ifdef HAVE_LINUX_FS_H
+#  include <linux/fs.h>
+# endif
 #endif
 
 #include <sys/user.h>

commit 59fa7b358290571f164701b96d0bac571c094749

fsmount.h: Try to fix mount includes
In file included from fsconfig01.c:9:
../../../../include/lapi/fsmount.h:55:8: error: redefinition of 'struct mount_attr'
   55 | struct mount_attr {
      |        ^~~~~~~~~~
In file included from ../../../../include/lapi/fsmount.h:14:
/home/autobuild/autobuild/instance-4/output-1/host/armeb-buildroot-linux-gnueabi/sysroot/usr/include/sys/mount.h:210:8: note: originally defined here
  210 | struct mount_attr
      |        ^~~~~

Link: http://autobuild.buildroot.org/results/4dbb72e1bf081afd3cd944571b9beeefc7608865
Fixes: b857f8723 ("lapi/fsmount: resolve conflict in different header files")
Reported-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/lapi/fsmount.h
@@ -13,10 +13,8 @@
 #include <sys/types.h>
 #include <sys/mount.h>
 
-#ifndef HAVE_FSOPEN
-# ifdef HAVE_LINUX_MOUNT_H
-#  include <linux/mount.h>
-# endif
+#ifdef HAVE_LINUX_MOUNT_H
+# include <linux/mount.h>
 #endif
 
 #include "lapi/fcntl.h"