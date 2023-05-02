142ebc18fffd9f1dee24703cd478d75abef04683
Petr Vorel <petr.vorel@gmail.com>
2023-05-02 16:43:07 +0200

    nfs08.sh: Skip on vfat, exfat
    vfat and exfat and exfa does not see '2' on various distros:
    * openSUSE Tumbleweed 20230427 (kernel 6.2.12-1-default, nfs-utils 2.6.3,
      mkfs.fat 4.2 (2021-01-31))
    * Debian 12 bookworm (kernel 6.1.0-6-amd64, nfs-utils 2.6.2, mkfs.fat 4.2
      (2021-01-31))
    
    NOTE: on it fails completely (on all filesystems) on Debian 11 bullseye
    (kernel 5.10.0-8-amd64, nfs-utils 1.3.3, mkfs.fat 4.2 (2021-01-31)) -
    likely due 1.3.3, thus skip the test completely.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/network/nfs/nfs_stress/nfs08.sh
+++ testcases/network/nfs/nfs_stress/nfs08.sh
@@ -8,6 +8,22 @@
 # Based on reproducer from Neil Brown <neilb@suse.de>
 
 TST_TESTFUNC="do_test"
+TST_SKIP_FILESYSTEMS="vfat,exfat"
+TST_SETUP="do_setup"
+
+do_setup()
+{
+	local util_version
+
+	nfs_setup
+
+	util_version=$(mount.nfs -V | sed 's/.*nfs-utils \([0-9]\)\..*/\1/')
+	if ! tst_is_int "$util_version"; then
+		tst_res TWARN "Failed to detect mount.nfs major version"
+	elif [ "$util_version" -lt 2 ]; then
+		tst_brk TCONF "Testing requires nfs-utils > 1"
+	fi
+}
 
 do_test()
 {
