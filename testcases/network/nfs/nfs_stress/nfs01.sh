6f836b5526d4db533a689ca8f7b589c106e0e055
Petr Vorel <pvorel@suse.cz>
2023-05-26 10:06:57 +0200

    nfs01.sh: Debug
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/network/nfs/nfs_stress/nfs01.sh
+++ testcases/network/nfs/nfs_stress/nfs01.sh
@@ -9,11 +9,36 @@
 # Ported by Robbie Williamson (robbiew@us.ibm.com)
 
 TST_TESTFUNC="do_test"
+TST_CLEANUP="do_cleanup"
+
+do_cleanup()
+{
+	echo "pids: '$pids'" >&2 # FIXME: debug
+	[ -n "$pids" ] && kill -9 $pids
+	nfs_cleanup
+}
 
 do_test()
 {
+	local n=0
+	local pids
+
 	tst_res TINFO "starting 'nfs01_open_files $NFILES'"
-	ROD nfs01_open_files $NFILES
+
+	# ROD nfs01_open_files $NFILES
+	for i in $VERSION; do
+		nfs01_open_files $NFILES &
+		pids="$pids $!"
+		n=$(( n + 1 ))
+	done
+
+	tst_res TINFO "waiting for pids:$pids"
+	for p in $pids; do
+		wait $p || tst_brk TFAIL "nfs01_open_files process failed"
+		tst_res TINFO "fsstress '$p' completed"
+	done
+	pids=
+
 	tst_res TPASS "test finished successfully"
 }
 
