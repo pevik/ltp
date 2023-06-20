pevik/ltp

commit d3c6120100352f831f86e98e5ec82bed15108b73

ptrace07: Fix compilation when cpuid.h is missing
This file is absent on Debian on some non-x86 arches. So we need to
check for it before including the file. Also we need to ensure we are
including the lapi version in the test.

Signed-off-by: Richard Palethorpe <rpalethorpe@suse.com>

include/lapi/cpuid.h
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include <cpuid.h>
+#ifdef HAVE_CPUID_H
+#  include <cpuid.h>
+#endif
 
 #ifndef LAPI_CPUID_H__
 #define LAPI_CPUID_H__

configure.ac
@@ -41,6 +41,7 @@ AC_CHECK_DECLS([SEM_STAT_ANY],,,[#include <sys/sem.h>])
 
 AC_CHECK_HEADERS_ONCE([ \
     asm/ldt.h \
+    cpuid.h \
     emmintrin.h \
     ifaddrs.h \
     keyutils.h \

testcases/kernel/syscalls/ptrace/ptrace07.c
@@ -35,7 +35,7 @@
 #include "config.h"
 #include "ptrace.h"
 #include "tst_test.h"
-#include "cpuid.h"
+#include "lapi/cpuid.h"
 
 #ifndef PTRACE_GETREGSET
 # define PTRACE_GETREGSET 0x4204

commit 256927edba9d45998a52c3d993809138974a43c6

lapi/cpuid.h: Include config.h
Lapi headers should always include it to be able to check #ifdef
HAVE_foo_H or other definitions.

Fixes: d3c612010 ("ptrace07: Fix compilation when cpuid.h is missing")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/lapi/cpuid.h
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#ifndef LAPI_CPUID_H__
+#define LAPI_CPUID_H__
+
+#include "config.h"
+
 #if !(defined(__i386__) || defined(__x86_64__))
 #  error "cpuid.h should only be included on x86"
 #endif
@@ -7,9 +12,6 @@
 #ifdef HAVE_CPUID_H
 #  include <cpuid.h>
 #endif
-
-#ifndef LAPI_CPUID_H__
-#define LAPI_CPUID_H__
 
 /*
  * gcc cpuid.h provides __cpuid_count() since v4.4.