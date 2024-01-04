1918256e32fdfa1f53df1cfa6ff935725930ca42
Petr Vorel <pvorel@suse.cz>
2024-01-04 19:52:51 +0100

    metadata: test.sh: Print more info on VERBOSE=1
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- metadata/tests/test.sh
+++ metadata/tests/test.sh
@@ -4,12 +4,17 @@ fail=0
 
 for i in *.c; do
 	../metaparse $i > tmp.json
+	[ "$VERBOSE" ] && echo "***** Parsing $i *****"
+
 	if ! diff tmp.json $i.json >/dev/null 2>&1; then
 		echo "***"
 		echo "$i output differs!"
 		diff -u tmp.json $i.json
 		echo "***"
 		fail=1
+		[ "$VERBOSE" ] && echo "$i: FAIL"
+	else
+		[ "$VERBOSE" ] && echo "$i: PASS"
 	fi
 done
 
