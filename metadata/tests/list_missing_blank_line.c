1570cfdbf62a69ad263976fddada957dfd435589
Petr Vorel <pvorel@suse.cz>
2024-01-04 20:07:39 +0100

    metadata: Add test for missing blank line in list
    This is a test for a fix in previous commit.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ metadata/tests/list_missing_blank_line.c
@@ -0,0 +1,14 @@
+/*\
+ * [Description]
+ *
+ * Some description.
+ *
+ * My BROKEN list (missing blank line before first item):
+ * * foo
+ * * bar
+ *
+ * My CORRECT list (with extra blank line before first item):
+ *
+ * * foo2
+ * * bar2
+ */
