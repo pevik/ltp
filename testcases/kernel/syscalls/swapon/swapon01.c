161534f737395177a3296c51b354a0769acb5312
Petr Vorel <pvorel@suse.cz>
2024-01-19 15:19:26 +0100

    swapon01: Test on all filesystems
    Test on all filesystems to increase coverage.
    Skip filesystems which does not support swap (currently bcachefs, btrfs
    and tmpfs).
    
    Tested on 5.10, 6.6 and 6.7.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/swapon/swapon01.c
+++ testcases/kernel/syscalls/swapon/swapon01.c
@@ -8,6 +8,7 @@
  * [Description]
  *
  * Checks that swapon() succeds with swapfile.
+ * Testing on all filesystems which support swap file.
  */
 
 #include <unistd.h>
@@ -17,13 +18,14 @@
 #include "lapi/syscalls.h"
 #include "libswap.h"
 
-#define SWAP_FILE "swapfile01"
+#define MNTPOINT	"mntpoint"
+#define SWAP_FILE	MNTPOINT"/swapfile01"
 
 static void verify_swapon(void)
 {
 	TST_EXP_PASS(tst_syscall(__NR_swapon, SWAP_FILE, 0));
 
-	if (TST_PASS && tst_syscall(__NR_swapoff, SWAP_FILE) != 0) {
+	if (tst_syscall(__NR_swapoff, SWAP_FILE) != 0) {
 		tst_brk(TBROK | TERRNO,
 				"Failed to turn off swapfile, system reboot recommended");
 	}
@@ -31,13 +33,20 @@ static void verify_swapon(void)
 
 static void setup(void)
 {
-	is_swap_supported(SWAP_FILE);
 	make_swapfile(SWAP_FILE, 0);
 }
 
 static struct tst_test test = {
-	.needs_root = 1,
-	.needs_tmpdir = 1,
+	.mntpoint = MNTPOINT,
+	.mount_device = 1,
+	.needs_root = 1, /* for swapon() */
+	.all_filesystems = 1,
 	.test_all = verify_swapon,
-	.setup = setup
+	.setup = setup,
+	.skip_filesystems = (const char *[]) {
+		"bcachefs",
+		"btrfs",
+		"tmpfs",
+		NULL
+	},
 };
