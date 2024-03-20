9ca14d982bf9fc0126c9cda604c96183062faf64
Andrea Cervesato <andrea.cervesato@suse.com>
2024-03-20 17:20:54 +0100

    Introduce kernel-doc API support
    kernel-doc API support is introduced by linuxdoc package that includes
    Linux kernel tools used to generate the documentation.
    With this patch we fully support the kernel documentation format that
    can be found here:
    
    https://return42.github.io/linuxdoc/linuxdoc-howto/kernel-doc-syntax.html

--- doc_new/conf.py
+++ doc_new/conf.py
@@ -14,8 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+extensions = ['linuxdoc.rstKernelDoc']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
