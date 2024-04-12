739912f7d9ec28dd7ae8311bcf65d27f0d7abcf1
Petr Vorel <pvorel@suse.cz>
2024-04-12 08:25:12 +0200

    lib: Add SAFE_FALLOCATE()
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- include/lapi/fallocate.h
+++ include/lapi/fallocate.h
@@ -7,6 +7,8 @@
 #ifndef LAPI_FALLOCATE_H__
 #define LAPI_FALLOCATE_H__
 
+#define _GNU_SOURCE
+
 #include "config.h"
 #include <sys/types.h>
 #include <endian.h>
@@ -15,6 +17,9 @@
 #include "lapi/fcntl.h"
 #include "lapi/seek.h"
 #include "lapi/syscalls.h"
+
+#define TST_NO_DEFAULT_MAIN
+#include "tst_test.h"
 
 #ifndef FALLOC_FL_KEEP_SIZE
 # define FALLOC_FL_KEEP_SIZE 0x01
@@ -51,5 +56,38 @@ static inline long fallocate(int fd, int mode, loff_t offset, loff_t len)
 # endif
 }
 #endif /* HAVE_FALLOCATE */
+
+#define SAFE_FALLOCATE(fd, mode, offset, len) \
+	safe_fallocate(__FILE__, __LINE__, (path), (mode), (offset), (len), #mode)
+
+/*
+ * static inlinde due off_t, see tst_safe_macros_inline.h.
+ * Kept separate because fallocate() requires _GNU_SOURCE.
+ */
+static inline int safe_fallocate(const char *file, const int lineno,
+	int fd, int mode, off_t offset, off_t len, const char *smode)
+{
+	int rval;
+
+	rval = fallocate(fd, mode, offset, len);
+
+	if (rval == -1) {
+		if (tst_fs_type(".") == TST_NFS_MAGIC && (errno == EOPNOTSUPP ||
+							  errno == ENOSYS)) {
+			tst_brk_(file, lineno, TCONF | TERRNO,
+					 "fallocate(%d, %s, %ld, %ld) unsupported",
+					 fd, smode, (long)offset, (long)len);
+		}
+		tst_brk_(file, lineno, TBROK | TERRNO,
+				 "fallocate(%d, %s, %ld, %ld) failed",
+				 fd, smode, (long)offset, (long)len);
+	} else if (rval < 0) {
+		tst_brk_(file, lineno, TBROK | TERRNO,
+			"Invalid fallocate(%d, %s, %ld, %ld) return value %d",
+				 fd, smode, (long)offset, (long)len, rval);
+	}
+
+	return rval;
+}
 
 #endif /* LAPI_FALLOCATE_H__ */
