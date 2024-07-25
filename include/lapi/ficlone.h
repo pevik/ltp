pevik/ltp

commit 3acc96d4b3fa8739a32be255a24ffd2b03350800

Add fallback definitions for lapi/fs.h
This patch adds "struct file_clone_range" and FICLONERANGE fallback
definitions.

Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

include/lapi/fs.h
@@ -18,7 +18,17 @@
 
 #include <sys/user.h>
 #include <limits.h>
+#include <stdint.h>
 #include "lapi/abisize.h"
+
+#ifndef HAVE_FILE_CLONE_RANGE
+struct file_clone_range {
+	int64_t src_fd;
+	uint64_t src_offset;
+	uint64_t src_length;
+	uint64_t dest_offset;
+};
+#endif
 
 #ifndef FS_IOC_GETFLAGS
 # define	FS_IOC_GETFLAGS	_IOR('f', 1, long)
@@ -46,6 +56,14 @@
 
 #ifndef FS_VERITY_FL
 # define FS_VERITY_FL	   0x00100000 /* Verity protected inode */
+#endif
+
+#ifndef FICLONE
+# define FICLONE		_IOW(0x94, 9, int)
+#endif
+
+#ifndef FICLONERANGE
+# define FICLONERANGE		_IOW(0x94, 13, struct file_clone_range)
 #endif
 
 /*

configure.ac
@@ -178,6 +178,7 @@ AC_CHECK_TYPES([struct acct_v3],,,[#include <sys/acct.h>])
 AC_CHECK_TYPES([struct af_alg_iv, struct sockaddr_alg],,,[# include <linux/if_alg.h>])
 AC_CHECK_TYPES([struct fanotify_event_info_fid, struct fanotify_event_info_error,
 		struct fanotify_event_info_header, struct fanotify_event_info_pidfd],,,[#include <sys/fanotify.h>])
+AC_CHECK_TYPES([struct file_clone_range],,,[#include <linux/fs.h>])
 AC_CHECK_TYPES([struct file_dedupe_range],,,[#include <linux/fs.h>])
 
 AC_CHECK_TYPES([struct file_handle],,,[

commit 01a1003d7472ce6f3e25a7d2157481a5b7b04df1

lapi: Split ficlone fallbacks from fs.h
In the end splitting the ficlone fallbacks from fs.h is the easiest
solution we can do.

Link: https://lore.kernel.org/ltp/20240725094300.16080-1-chrubis@suse.cz/
Fixes: 3acc96d4b3fa8 ("Add fallback definitions for lapi/fs.h")
Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

include/lapi/ficlone.h
@@ -1,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2024 Andrea Cervesato <andrea.cervesato@suse.com>
+ * Copyright (C) 2024 Cyril Hrubis <chrubis@suse.cz>
+ */
+
+#ifndef LAPI_FICLONE_H__
+#define LAPI_FICLONE_H__
+
+#include "config.h"
+
+#ifdef HAVE_LINUX_FS_H
+# include <linux/fs.h>
+#endif
+
+#include <stdint.h>
+
+#ifndef HAVE_STRUCT_FILE_CLONE_RANGE
+struct file_clone_range {
+	int64_t src_fd;
+	uint64_t src_offset;
+	uint64_t src_length;
+	uint64_t dest_offset;
+};
+#endif
+
+#ifndef FICLONE
+# define FICLONE		_IOW(0x94, 9, int)
+#endif
+
+#ifndef FICLONERANGE
+# define FICLONERANGE		_IOW(0x94, 13, struct file_clone_range)
+#endif
+
+#endif /* LAPI_FICLONE_H__ */

include/lapi/fs.h
@@ -18,17 +18,7 @@
 
 #include <sys/user.h>
 #include <limits.h>
-#include <stdint.h>
 #include "lapi/abisize.h"
-
-#ifndef HAVE_STRUCT_FILE_CLONE_RANGE
-struct file_clone_range {
-	int64_t src_fd;
-	uint64_t src_offset;
-	uint64_t src_length;
-	uint64_t dest_offset;
-};
-#endif
 
 #ifndef FS_IOC_GETFLAGS
 # define	FS_IOC_GETFLAGS	_IOR('f', 1, long)
@@ -56,14 +46,6 @@ struct file_clone_range {
 
 #ifndef FS_VERITY_FL
 # define FS_VERITY_FL	   0x00100000 /* Verity protected inode */
-#endif
-
-#ifndef FICLONE
-# define FICLONE		_IOW(0x94, 9, int)
-#endif
-
-#ifndef FICLONERANGE
-# define FICLONERANGE		_IOW(0x94, 13, struct file_clone_range)
 #endif
 
 /*

testcases/kernel/syscalls/ioctl/ioctl_ficlone01.c
@@ -19,7 +19,7 @@
  */
 
 #include "tst_test.h"
-#include "lapi/fs.h"
+#include "lapi/ficlone.h"
 
 #define MNTPOINT "mnt"
 #define SRCPATH MNTPOINT "/file0"

testcases/kernel/syscalls/ioctl/ioctl_ficlone02.c
@@ -12,7 +12,7 @@
  */
 
 #include "tst_test.h"
-#include "lapi/fs.h"
+#include "lapi/ficlone.h"
 
 #define MNTPOINT "mnt"
 #define SRCPATH MNTPOINT "/file0"

testcases/kernel/syscalls/ioctl/ioctl_ficlone03.c
@@ -11,7 +11,7 @@
  */
 
 #include "tst_test.h"
-#include "lapi/fs.h"
+#include "lapi/ficlone.h"
 
 #define MNTPOINT "mnt"
 

testcases/kernel/syscalls/ioctl/ioctl_ficlonerange01.c
@@ -19,7 +19,7 @@
  */
 
 #include "tst_test.h"
-#include "lapi/fs.h"
+#include "lapi/ficlone.h"
 
 #define MNTPOINT "mnt"
 #define SRCPATH MNTPOINT "/file0"

testcases/kernel/syscalls/ioctl/ioctl_ficlonerange02.c
@@ -13,7 +13,7 @@
  */
 
 #include "tst_test.h"
-#include "lapi/fs.h"
+#include "lapi/ficlone.h"
 
 #define MNTPOINT "mnt"
 #define SRCPATH MNTPOINT "/file0"