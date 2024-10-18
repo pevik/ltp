778739e79669c2a80c082243c2ee2c19eade0b63
Ricardo B. Marliere <rbm@suse.com>
2024-10-18 15:33:56 -0300

    block_dev: Convert to new API
    Signed-off-by: Ricardo B. Marliere <rbm@suse.com>

--- testcases/kernel/device-drivers/block/block_dev_user/block_dev.c
+++ testcases/kernel/device-drivers/block/block_dev_user/block_dev.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2013 Oracle and/or its affiliates. All Rights Reserved.
  *
@@ -26,74 +27,59 @@
 #include <unistd.h>
 #include <string.h>
 
-#include "test.h"
-#include "safe_macros.h"
-#include "old_module.h"
+#include "tst_test.h"
+#include "tst_module.h"
+
+#define MODULE_NAME "ltp_block_dev"
+#define MODULE_NAME_KO	MODULE_NAME ".ko"
 
 char *TCID = "block_dev";
-int TST_TOTAL = 9;
 
-static const char module_name[]	= "ltp_block_dev.ko";
 static const char dev_result[]	= "/sys/devices/ltp_block_dev/result";
 static const char dev_tcase[]	= "/sys/devices/ltp_block_dev/tcase";
-static int module_loaded;
 
-static int run_all_testcases;
-static const option_t options[] = {
-	{"a", &run_all_testcases, NULL},
+static int module_loaded;
+static char* run_all_testcases;
+static struct tst_option options[] = {
+	{"a",  &run_all_testcases, "-a\tRun all test-cases (can crash the kernel)"},
 	{NULL, NULL, NULL}
 };
 
 static void cleanup(void)
 {
 	if (module_loaded)
-		tst_module_unload(NULL, module_name);
+		tst_module_unload(MODULE_NAME_KO);
 }
 
-static void help(void)
+static void run(unsigned int n)
 {
-	printf("  -a      Run all test-cases (can crash the kernel)\n");
-}
-
-void setup(int argc, char *argv[])
-{
-	tst_parse_opts(argc, argv, options, help);
-
-	tst_require_root();
-
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-}
-
-static void test_run(void)
-{
-	int off = 0;
 	/*
 	 * test-cases #8 and #9 can crash the kernel.
 	 * We have to wait for kernel fix where register_blkdev() &
 	 * unregister_blkdev() checks the input device name parameter
 	 * against NULL pointer.
 	 */
-	if (!run_all_testcases)
-		off = 2;
-
-	tst_module_load(cleanup, module_name, NULL);
-	module_loaded = 1;
-
-	int i, pass = 0;
-	for (i = 0; i < TST_TOTAL - off; ++i) {
-		SAFE_FILE_PRINTF(cleanup, dev_tcase, "%d", i + 1);
-		SAFE_FILE_SCANF(cleanup, dev_result, "%d", &pass);
-		tst_resm((pass) ? TPASS : TFAIL, "Test-case '%d'", i + 1);
+	n++;
+	if (!run_all_testcases && (n == 8 || n == 9)) {
+		tst_res(TCONF, "Skipped n = %d", n);
+		return;
 	}
+
+	if (!module_loaded) {
+		tst_module_load(MODULE_NAME_KO, NULL);
+		module_loaded = 1;
+	}
+
+	int pass = 0;
+	SAFE_FILE_PRINTF(dev_tcase, "%d", n);
+	SAFE_FILE_SCANF(dev_result, "%d", &pass);
+	tst_res((pass) ? TPASS : TFAIL, "Test-case '%d'", n);
 }
 
-int main(int argc, char *argv[])
-{
-	setup(argc, argv);
-
-	test_run();
-
-	cleanup();
-
-	tst_exit();
-}
+static struct tst_test test = {
+	.needs_root = 1,
+	.cleanup = cleanup,
+	.test = run,
+	.tcnt = 9,
+	.options = options,
+};
