pevik/ltp

commit 7248e5c5f780ad9026dff1ad055b6289254fd9d7

doc: update syscalls statistics
This patch black-list a few syscalls which are not implemented in the
kernel, untestable or either really old (=< 2.6 kernel version), as well
as defining already tested syscalls.

Also fixed an index issue with table generation.

Link: https://lore.kernel.org/ltp/20240423082119.16117-1-andrea.cervesato@suse.de/
Reviewed-by: Petr Vorel <pvorel@suse.cz>
Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

doc/conf.py
@@ -76,14 +76,31 @@ def generate_syscalls_stats(_):
         'io_uring_register',
         'epoll_pwait2',
         'quotactl_fd',
+        'pread64',
+        'pwrite64',
+        'fadvise64',
+        'getmsg',
+        'getpmsg',
+        'putmsg',
+        'putpmsg',
     ]
 
-    # populate with non-syscalls which are present in the kernel sources
-    # syscalls file
+    # populate with not implemented, reserved, unmaintained syscalls defined
+    # inside the syscalls file
     black_list = [
         'reserved177',
         'reserved193',
-        'rseq'
+        'rseq',
+        '_newselect',
+        '_sysctl',
+        'create_module',
+        'get_kernel_syms',
+        'query_module',
+        'nfsservctl',
+        'afs_syscall',
+        'sysmips',
+        'mq_getsetattr',
+        'vserver',
     ]
 
     # fetch syscalls file
@@ -184,12 +201,12 @@ def generate_syscalls_stats(_):
 
     left = index_tested % 3
     if left > 0:
-        for index in range(0, left + 1):
+        for index in range(0, 3 - left):
             table_tested.append(f'      -\n')
 
     left = index_untest % 3
     if left > 0:
-        for index in range(0, left + 1):
+        for index in range(0, 3 - left):
             table_untest.append(f'      -\n')
 
     text.extend(table_tested)

commit d3ac00e208bae4f76ac079c67ebe29d6647cfff0

doc: Update blacklist and whitelist
getmsg, getpmsg, putmsg, putpmsg in man UNIMPLEMENTED(2),
move them from whitelist to blacklist.

seccomp is now detected, therefore remove from whitelist.

Fixes: 7248e5c5f7 ("doc: update syscalls statistics")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

doc/conf.py
@@ -67,8 +67,6 @@ def generate_syscalls_stats(_):
         'fanotify_init',
         'fanotify_mark',
         'getdents64',
-        'getmsg',
-        'getpmsg',
         'inotify_add_watch',
         'inotify_rm_watch',
         'io_uring_enter',
@@ -79,8 +77,6 @@ def generate_syscalls_stats(_):
         'landlock_restrict_self',
         'lsetxattr',
         'newfstatat',
-        'putmsg',
-        'putpmsg',
         'pkey_alloc',
         'pkey_free',
         'pkey_mprotect',
@@ -90,7 +86,6 @@ def generate_syscalls_stats(_):
         'pwrite64',
         'quotactl_fd',
         'rt_sigpending',
-        'seccomp',
         'semtimedop',
         'sethostname',
     ]