6ba6a00445816627656d9c4a8f1911a930476f54
Ricardo B. Marlière <rbm@suse.com>
2025-03-31 14:43:47 -0300

    syscalls/modify_ldt: Add lapi/ldt.h
    Add a wrapper to modify_ldt which is used in a few tests and should be
    reused.
    
    Reviewed-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Signed-off-by: Ricardo B. Marlière <rbm@suse.com>

---
+++ include/lapi/ldt.h
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2025 SUSE LLC Ricardo B. Marlière <rbm@suse.com>
+ */
+
+#ifndef LAPI_LDT_H__
+#define LAPI_LDT_H__
+
+#include "config.h"
+#include "lapi/syscalls.h"
+#include <asm/ldt.h>
+
+static int modify_ldt(int func, void *ptr, unsigned long bytecount)
+{
+	return tst_syscall(__NR_modify_ldt, func, ptr, bytecount);
+}
+
+static int safe_modify_ldt(const char *file, const int lineno, int func,
+			   void *ptr, unsigned long bytecount)
+{
+	int rval;
+
+	rval = modify_ldt(func, ptr, bytecount);
+	if (rval == -1)
+		tst_brk_(file, lineno, TBROK | TERRNO,
+			 "modify_ldt(%d, %p, %lu)", func, ptr, bytecount);
+
+	return rval;
+}
+
+#define SAFE_MODIFY_LDT(func, ptr, bytecount) \
+	safe_modify_ldt(__FILE__, __LINE__, (func), (ptr), (bytecount))
+
+#endif /* LAPI_LDT_H__ */
