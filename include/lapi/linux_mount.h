pevik/ltp

commit 5c5411ea8ef9f71792f06c9a6f59c32463fd3d7c

Add listmount/statmount fallback declarations
Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

configure.ac
@@ -246,6 +246,8 @@ AC_CHECK_TYPES([struct mount_attr],,,[
 
 AC_CHECK_TYPES([struct cachestat_range],,,[#include <sys/mman.h>])
 AC_CHECK_TYPES([struct cachestat],,,[#include <sys/mman.h>])
+AC_CHECK_TYPES([struct mnt_id_req],,,[#include <linux/mount.h>])
+AC_CHECK_TYPES([struct statmount],,,[#include <linux/mount.h>])
 
 # Tools knobs
 

include/lapi/mount.h
@@ -2,12 +2,15 @@
 /*
  * Copyright (c) Linux Test Project, 2015-2022
  * Copyright (c) 2015 Cui Bixuan <cuibixuan@huawei.com>
+ * Copyright (C) 2024 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
 
 #ifndef LAPI_MOUNT_H__
 #define LAPI_MOUNT_H__
 
+#include <stdint.h>
 #include <sys/mount.h>
+#include "config.h"
 
 #ifndef MS_REC
 # define MS_REC 16384
@@ -35,6 +38,73 @@
 
 #ifndef MS_NOSYMFOLLOW
 # define MS_NOSYMFOLLOW 256
+#endif
+
+#ifndef HAVE_STRUCT_MNT_ID_REQ
+struct mnt_id_req {
+	uint32_t size;
+	uint32_t spare;
+	uint64_t mnt_id;
+	uint64_t param;
+};
+#endif
+
+#ifndef HAVE_STRUCT_STATMOUNT
+struct statmount {
+	uint32_t size;
+	uint32_t __spare1;
+	uint64_t mask;
+	uint32_t sb_dev_major;
+	uint32_t sb_dev_minor;
+	uint64_t sb_magic;
+	uint32_t sb_flags;
+	uint32_t fs_type;
+	uint64_t mnt_id;
+	uint64_t mnt_parent_id;
+	uint32_t mnt_id_old;
+	uint32_t mnt_parent_id_old;
+	uint64_t mnt_attr;
+	uint64_t mnt_propagation;
+	uint64_t mnt_peer_group;
+	uint64_t mnt_master;
+	uint64_t propagate_from;
+	uint32_t mnt_root;
+	uint32_t mnt_point;
+	uint64_t __spare2[50];
+	char str[];
+};
+#endif
+
+#ifndef MNT_ID_REQ_SIZE_VER0
+# define MNT_ID_REQ_SIZE_VER0 24
+#endif
+
+#ifndef STATMOUNT_SB_BASIC
+# define STATMOUNT_SB_BASIC 0x00000001U
+#endif
+
+#ifndef STATMOUNT_MNT_BASIC
+# define STATMOUNT_MNT_BASIC 0x00000002U
+#endif
+
+#ifndef STATMOUNT_PROPAGATE_FROM
+# define STATMOUNT_PROPAGATE_FROM 0x00000004U
+#endif
+
+#ifndef STATMOUNT_MNT_ROOT
+# define STATMOUNT_MNT_ROOT 0x00000008U
+#endif
+
+#ifndef STATMOUNT_MNT_POINT
+# define STATMOUNT_MNT_POINT 0x00000010U
+#endif
+
+#ifndef STATMOUNT_FS_TYPE
+# define STATMOUNT_FS_TYPE 0x00000020U
+#endif
+
+#ifndef LSMT_ROOT
+# define LSMT_ROOT 0xffffffffffffffff
 #endif
 
 #endif /* LAPI_MOUNT_H__ */

commit 7f5fd8d0e3ed62b742b799f4d383ebfff8886041

lapi: Introduce {linux,sys}_mount.h to fix MUSL on 6.14 headers
Due indirect include <linux/mount.h> by <linux/fs.h> we really need to
separate old and new mount API. This fixes error:

This fixes CI build on Alpine v3.22 (the default Alpine version in
GitHub action), which uses 6.14.2 kernel headers:

In file included from /usr/include/linux/fs.h:19,
                 from /usr/include/linux/btrfs.h:29,
                 from statmount02.c:23:
/usr/include/linux/mount.h:155:8: error: redefinition of 'struct statmount'
  155 | struct statmount {
      |        ^~~~~~~~~
In file included from statmount.h:12,
                 from statmount02.c:20:
../../../../include/lapi/mount.h:58:8: note: originally defined here
   58 | struct statmount {
      |        ^~~~~~~~~
/usr/include/linux/mount.h:193:8: error: redefinition of 'struct mnt_id_req'
  193 | struct mnt_id_req {
      |        ^~~~~~~~~~
../../../../include/lapi/mount.h:49:8: note: originally defined here
   49 | struct mnt_id_req {
      |        ^~~~~~~~~~

But we still support old Leap 42 (glibc 2.22 based), which requires for
statmount04.c old fallbacks for <sys/mount.h> but also new mount API
defined in <linux/mount.h>, otherwise it fails:

statmount03.c:62:4: error: 'MS_PRIVATE' undeclared here (not in a function)
  { MS_PRIVATE, TST_TO_STR_(MS_PRIVATE) },
    ^
statmount03.c:63:4: error: 'MS_SHARED' undeclared here (not in a function)
  { MS_SHARED, TST_TO_STR_(MS_SHARED) },
    ^
statmount03.c:64:4: error: 'MS_SLAVE' undeclared here (not in a function)
  { MS_SLAVE, TST_TO_STR_(MS_SLAVE) },
    ^
In file included from ../../../../include/tst_test.h:185:0,
                 from statmount.h:11,
                 from statmount04.c:21:
statmount04.c: In function 'setup':
statmount03.c:65:4: error: 'MS_UNBINDABLE' undeclared here (not in a function)
  { MS_UNBINDABLE, TST_TO_STR_(MS_UNBINDABLE) },
    ^
statmount04.c:57:35: error: 'MS_BIND' undeclared (first use in this function)
  SAFE_MOUNT(DIR_A, DIR_A, "none", MS_BIND, NULL);
                                   ^
../../../../include/tst_safe_macros.h:244:25: note: in definition of macro 'SAFE_MOUNT'
      (filesystemtype), (mountflags), (data))
                         ^
In file included from ../../../../include/tst_test.h:185:0,
                 from statmount.h:11,
                 from statmount03.c:21:
statmount03.c: In function 'run':
statmount04.c:57:35: note: each undeclared identifier is reported only once for each function it appears in
  SAFE_MOUNT(DIR_A, DIR_A, "none", MS_BIND, NULL);
                                   ^
../../../../include/tst_safe_macros.h:244:25: note: in definition of macro 'SAFE_MOUNT'
      (filesystemtype), (mountflags), (data))
                         ^
statmount03.c:74:35: error: 'MS_BIND' undeclared (first use in this function)
  SAFE_MOUNT(DIR_B, DIR_A, "none", MS_BIND, NULL);

Fixes: 5c5411ea8e ("Add listmount/statmount fallback declarations")

configure.ac
@@ -254,10 +254,8 @@ AC_CHECK_TYPES([struct mount_attr],,,[
 
 AC_CHECK_TYPES([struct cachestat_range],,,[#include <sys/mman.h>])
 AC_CHECK_TYPES([struct cachestat],,,[#include <sys/mman.h>])
-
-# Defined in <linux/mount.h>, but include/lapi/mount.h includes <sys/mount.h> */
-AC_CHECK_TYPES([struct mnt_id_req],,,[#include <sys/mount.h>])
-AC_CHECK_TYPES([struct statmount],,,[#include <sys/mount.h>])
+AC_CHECK_TYPES([struct mnt_id_req],,,[#include <linux/mount.h>])
+AC_CHECK_TYPES([struct statmount],,,[#include <linux/mount.h>])
 
 # Tools knobs
 

include/lapi/linux_mount.h
@@ -1,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) Linux Test Project, 2015-2025
+ */
+
+#ifndef LINUX_LAPI_MOUNT_H__
+#define LINUX_LAPI_MOUNT_H__
+
+/*
+ * NOTE: <linux/mount.h> conflicts with <sys/mount.h>, therefore all fallback
+ * definitions from both of them should go to lapi/mount.h.
+ */
+#ifdef HAVE_LINUX_MOUNT_H
+# include <linux/mount.h>
+#endif
+
+#include "lapi/mount.h"
+
+#endif /* LINUX_LAPI_MOUNT_H__ */

include/lapi/mount.h
@@ -8,14 +8,13 @@
 #ifndef LAPI_MOUNT_H__
 #define LAPI_MOUNT_H__
 
+/*
+ * NOTE: <linux/mount.h> conflicts with <sys/mount.h>, therefore all fallback
+ * definitions from both of them should go to here.
+ */
+
 #include "config.h"
 #include <stdint.h>
-
-/*
- * NOTE: <linux/mount.h> conflicts with <sys/mount.h>, therefore not added
- * although some definitions from it are used.
- */
-#include <sys/mount.h>
 
 #ifndef MS_REC
 # define MS_REC 16384

include/lapi/sys_mount.h
@@ -1,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) Linux Test Project, 2025
+ */
+
+#ifndef SYS_MOUNT_H__
+#define SYS_MOUNT_H__
+
+/*
+ * NOTE: <linux/mount.h> conflicts with <sys/mount.h>, therefore all fallback
+ * definitions from both of them should go to lapi/mount.h.
+ */
+#include <sys/mount.h>
+#include "lapi/mount.h"
+
+#endif /* SYS_MOUNT_H__ */

testcases/kernel/syscalls/listmount/listmount.h
@@ -9,7 +9,7 @@
 #define _GNU_SOURCE
 
 #include "tst_test.h"
-#include "lapi/mount.h"
+#include "lapi/linux_mount.h"
 #include "lapi/syscalls.h"
 
 static inline ssize_t listmount(uint64_t mnt_id, uint64_t last_mnt_id,

testcases/kernel/syscalls/listmount/listmount04.c
@@ -15,7 +15,7 @@
 #define _GNU_SOURCE
 
 #include "tst_test.h"
-#include "lapi/mount.h"
+#include "lapi/linux_mount.h"
 #include "lapi/syscalls.h"
 
 #define MNT_SIZE 32

testcases/kernel/syscalls/mount/mount03.c
@@ -27,7 +27,7 @@
 #include <sys/vfs.h>
 #include <pwd.h>
 #include "tst_test.h"
-#include "lapi/mount.h"
+#include "lapi/sys_mount.h"
 
 #define MNTPOINT	"mntpoint"
 #define TESTBIN	"mount03_suid_child"

testcases/kernel/syscalls/mount/mount07.c
@@ -21,7 +21,7 @@
 #include <sys/mount.h>
 #include <stdbool.h>
 #include "tst_test.h"
-#include "lapi/mount.h"
+#include "lapi/sys_mount.h"
 
 #ifndef ST_NOSYMFOLLOW
 # define ST_NOSYMFOLLOW 0x2000

testcases/kernel/syscalls/open/open12.c
@@ -30,7 +30,7 @@
 #include "test.h"
 #include "safe_macros.h"
 #include "lapi/fcntl.h"
-#include "lapi/mount.h"
+#include "lapi/sys_mount.h"
 
 #define MNTPOINT	"mntpoint"
 #define TEST_FILE	MNTPOINT"/test_file"

testcases/kernel/syscalls/pivot_root/pivot_root01.c
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 #include "tst_test.h"
-#include "lapi/mount.h"
+#include "lapi/sys_mount.h"
 
 #ifdef HAVE_UNSHARE
 

testcases/kernel/syscalls/statmount/statmount.h
@@ -9,7 +9,7 @@
 #define _GNU_SOURCE
 
 #include "tst_test.h"
-#include "lapi/mount.h"
+#include "lapi/linux_mount.h"
 #include "lapi/syscalls.h"
 #include "tst_safe_stdio.h"
 

testcases/kernel/syscalls/statx/statx06.c
@@ -25,7 +25,7 @@
 #include "tst_safe_macros.h"
 #include "tst_timer.h"
 #include "lapi/stat.h"
-#include "lapi/mount.h"
+#include "lapi/sys_mount.h"
 #include "lapi/fcntl.h"
 
 #define MOUNT_POINT "mount_ext"

testcases/kernel/syscalls/umount2/umount2_01.c
@@ -26,7 +26,7 @@
 
 #include "test.h"
 #include "safe_macros.h"
-#include "lapi/mount.h"
+#include "lapi/sys_mount.h"
 
 static void setup(void);
 static void umount2_verify(void);

testcases/kernel/syscalls/umount2/umount2_02.c
@@ -18,8 +18,8 @@
  * - succeed when target is a mount point
  */
 
-#include "lapi/mount.h"
 #include "tst_test.h"
+#include "lapi/sys_mount.h"
 
 #define MNTPOINT        "mntpoint"
 #define SYMLINK	"symlink"