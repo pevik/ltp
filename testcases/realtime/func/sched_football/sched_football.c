798cdc7f83c034cca8eeabce48e5c0ff456b799c
Li Wang <liwang@redhat.com>
2025-09-04 18:26:09 +0800

    sched_football: synchronize with kickoff flag to reduce skew
    Previously, thread synchronization in sched_football only relied on a
    thread_barrier. This ensured that all threads were created before the
    referee started the game, but did not fully prevent offense threads from
    getting a scheduling opportunity before defense threads were migrated,
    leading to occasional non-zero final ball positions on kvm or debug kernels.
    
    This patch introduces an explicit `kickoff_flag`:
    
    * All player threads (offense, defense, fans) wait at the barrier and
      then spin until the referee sets `kickoff_flag`. This reduces kernel
      scheduling skew, as threads only proceed once the referee explicitly
      signals the kickoff.
    
    * The referee now:
      - Waits at the barrier.
      - Clears the ball position.
      - Sets `kickoff_flag` to start the ball.
    
    * Game termination is also slightly reordered (by Cyril):
      - Final ball position is read before `game_over` is set,
        avoiding a race where the ball could still move right after
        defense threads stop.
    
    This makes startup sequencing more deterministic while still allowing
    some nondeterminism, which is intentional for testing scheduler
    behavior under load.
    
    Signed-off-by: Li Wang <liwang@redhat.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>
    Tested-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    [ pvorel: Remove disablement for non-RT kernels ]
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/realtime/func/sched_football/sched_football.c
+++ testcases/realtime/func/sched_football/sched_football.c
@@ -44,6 +44,7 @@
 static tst_atomic_t the_ball;
 static int players_per_team = 0;
 static int game_length = DEF_GAME_LENGTH;
+static tst_atomic_t kickoff_flag;
 static tst_atomic_t game_over;
 
 static char *str_game_length;
@@ -55,6 +56,9 @@ void *thread_fan(void *arg LTP_ATTRIBUTE_UNUSED)
 {
 	prctl(PR_SET_NAME, "crazy_fan", 0, 0, 0);
 	pthread_barrier_wait(&start_barrier);
+	while (!tst_atomic_load(&kickoff_flag))
+		;
+
 	/*occasionally wake up and run across the field */
 	while (!tst_atomic_load(&game_over)) {
 		struct timespec start, stop;
@@ -80,6 +84,9 @@ void *thread_defense(void *arg LTP_ATTRIBUTE_UNUSED)
 {
 	prctl(PR_SET_NAME, "defense", 0, 0, 0);
 	pthread_barrier_wait(&start_barrier);
+	while (!tst_atomic_load(&kickoff_flag))
+		;
+
 	/*keep the ball from being moved */
 	while (!tst_atomic_load(&game_over)) {
 	}
@@ -92,6 +99,9 @@ void *thread_offense(void *arg LTP_ATTRIBUTE_UNUSED)
 {
 	prctl(PR_SET_NAME, "offense", 0, 0, 0);
 	pthread_barrier_wait(&start_barrier);
+	while (!tst_atomic_load(&kickoff_flag))
+		;
+
 	while (!tst_atomic_load(&game_over)) {
 		tst_atomic_add_return(1, &the_ball); /* move the ball ahead one yard */
 	}
@@ -115,9 +125,11 @@ void referee(int game_length)
 	now = start;
 
 	/* Start the game! */
-	tst_atomic_store(0, &the_ball);
-	pthread_barrier_wait(&start_barrier);
 	atrace_marker_write("sched_football", "Game_started!");
+	pthread_barrier_wait(&start_barrier);
+	tst_atomic_store(0, &the_ball);
+	tst_atomic_store(1, &kickoff_flag);
+	usleep(20000);
 
 	/* Watch the game */
 	while ((now.tv_sec - start.tv_sec) < game_length) {
@@ -125,13 +137,13 @@ void referee(int game_length)
 		gettimeofday(&now, NULL);
 	}
 
-	/* Stop the game! */
-	tst_atomic_store(1, &game_over);
-	atrace_marker_write("sched_football", "Game_Over!");
-
 	/* Blow the whistle */
 	final_ball = tst_atomic_load(&the_ball);
 	tst_res(TINFO, "Final ball position: %d", final_ball);
+
+	/* Stop the game! */
+	tst_atomic_store(1, &game_over);
+	atrace_marker_write("sched_football", "Game_Over!");
 
 	TST_EXP_EXPR(final_ball == 0);
 }
@@ -154,6 +166,7 @@ static void do_test(void)
 	/* We're the ref, so set our priority right */
 	param.sched_priority = sched_get_priority_min(SCHED_FIFO) + 80;
 	sched_setscheduler(0, SCHED_FIFO, &param);
+	tst_atomic_store(0, &kickoff_flag);
 
 	/*
 	 * Start the offense
@@ -186,6 +199,11 @@ static void do_test(void)
 
 static void do_setup(void)
 {
+	/*
+	if (!tst_check_preempt_rt())
+		tst_brk(TCONF, "Test requires real-time kernel");
+	*/
+
 	if (tst_parse_int(str_game_length, &game_length, 1, INT_MAX))
 		tst_brk(TBROK, "Invalid game length '%s'", str_game_length);
 
