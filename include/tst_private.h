pevik/ltp

commit 9bb94efa39bb1b08f37e56c7437db5fa13ddcae2

tst_cmd: Change tst_check_cmd() return type to int
Function use just bool value, no need for int (readability).

Link: https://lore.kernel.org/ltp/20251020142813.415231-1-pvorel@suse.cz/
Reviewed-by: Li Wang <liwang@redhat.com>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/tst_private.h
@@ -45,7 +45,7 @@ char tst_kconfig_get(const char *confname);
  * function checks command version whether meets this requirement.
  * If not, case breaks if brk_nosupp is defined.
  */
-int tst_check_cmd(const char *cmd, const int brk_nosupp);
+bool tst_check_cmd(const char *cmd, const int brk_nosupp);
 
 /*
  * Returns NULL-terminated array of kernel-supported filesystems.

lib/tst_cmd.c
@@ -27,6 +27,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdbool.h>
 #include "test.h"
 #include "tst_cmd.h"
 #include "tst_private.h"
@@ -249,7 +250,7 @@ static struct version_parser {
 	{},
 };
 
-int tst_check_cmd(const char *cmd, const int brk_nosupp)
+bool tst_check_cmd(const char *cmd, const int brk_nosupp)
 {
 	struct version_parser *p;
 	char *cmd_token, *op_token, *version_token, *next, *str;
@@ -268,7 +269,7 @@ int tst_check_cmd(const char *cmd, const int brk_nosupp)
 		tst_brkm(TCONF, NULL, "Couldn't find '%s' in $PATH", cmd_token);
 
 	if (!op_token)
-		return 0;
+		return true;
 
 	if (!version_token || str) {
 		tst_brkm(TCONF, NULL,
@@ -318,7 +319,7 @@ int tst_check_cmd(const char *cmd, const int brk_nosupp)
 		tst_brkm(TCONF, NULL, "Invalid op(%s)", op_token);
 	}
 
-	return 0;
+	return true;
 error:
 	if (brk_nosupp) {
 		tst_brkm(TCONF, NULL, "%s requires %s %d, but got %d",
@@ -328,5 +329,5 @@ error:
 			cmd, op_token, ver_get, ver_parser);
 	}
 
-	return 1;
+	return false;
 }

commit f2b43457f6db3b2f91a456875eecc7803356b77e

tst_test.c: Fix tst_check_cmd() use
9bb94efa39 changed tst_check_cmd() from int to bool. But it reverted the
values (0 became true), which logical (false means not supported) than
the original return codes from 257394e4e3. But then the only use of
tst_check_cmd() needs to update to expect false instead of true.

Also document the return code in tst_private.h.

Fixes: 9bb94efa39 ("tst_cmd: Change tst_check_cmd() return type to int")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/tst_private.h
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
+ * Copyright (c) Linux Test Project, 2020-2025
  * Copyright (c) 2017-2019 Petr Vorel <pvorel@suse.cz>
  *
  * Internal helper functions for the shell library. Do not use directly
@@ -41,9 +42,13 @@ char tst_kconfig_get(const char *confname);
 /*
  * If cmd argument is a single command, this function just checks command
  * whether exists. If not, case breaks if brk_nosupp is defined.
+ *
  * If cmd argument is a complex string ie 'mkfs.ext4 >= 1.43.0', this
  * function checks command version whether meets this requirement.
  * If not, case breaks if brk_nosupp is defined.
+ *
+ * return: true if command presented, optionally with high enough version, false
+ * otherwise.
  */
 bool tst_check_cmd(const char *cmd, const int brk_nosupp);
 

lib/tst_test.c
@@ -1960,7 +1960,7 @@ static int run_tcase_on_fs(struct tst_fs *fs, const char *fs_type)
 	tst_res(TINFO, "=== Testing on %s ===", fs_type);
 	tdev.fs_type = fs_type;
 
-	if (fs->mkfs_ver && tst_check_cmd(fs->mkfs_ver, 0))
+	if (fs->mkfs_ver && !tst_check_cmd(fs->mkfs_ver, 0))
 		return TCONF;
 
 	if (fs->min_kver && check_kver(fs->min_kver, 0))