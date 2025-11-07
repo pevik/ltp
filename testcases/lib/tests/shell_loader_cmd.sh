e1649b9aca491fe94c78a25427f14803a2d9afc4
Wei Gao <wegao@suse.com>
2025-11-07 00:30:27 +0000

    shell_loader_cmd.sh: New test case check needs_cmds
    Suggested-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Wei Gao <wegao@suse.com>

---
+++ testcases/lib/tests/shell_loader_cmd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2025 Wei Gao <wegao@suse.cz>
+#
+# ---
+# env
+# {
+#  "needs_cmds": [
+#   {
+#    "cmd": "ls",
+#    "optional": 1
+#   }
+#  ]
+# }
+# ---
+
+. tst_loader.sh
+
+tst_test()
+{
+	tst_res TPASS "We are running with needs_cmds"
+}
+
+. tst_run.sh
