93b9ccdfe159e38c2defea5c93d13dd295bea003
Petr Vorel <pvorel@suse.cz>
2026-06-03 08:20:34 +0200

    lapi: Move renameat2.h into lapi/stdio.h
    Fallback definition for renameat2() is currently needed at least for
    Musl < v1.2.6 (in current Alpine in CI) and openSUSE Leap 42.2
    (still being supported due the same SLES version being tested).
    
    testcases/kernel/syscalls/renameat2/renameat2.h was a test specific
    header which contains only fallback for renameat2(). Move it into
    include/lapi/stdio.h with minor cleanup.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/renameat2/renameat201.c
+++ testcases/kernel/syscalls/renameat2/renameat201.c
@@ -15,9 +15,9 @@
 
 #define _GNU_SOURCE
 
-#include "lapi/fcntl.h"
 #include "tst_test.h"
-#include "renameat2.h"
+#include "lapi/fcntl.h"
+#include "lapi/stdio.h"
 
 #define TEST_DIR "test_dir/"
 #define TEST_DIR2 "test_dir2/"
