e697b79678875ef822050858127bae98682a19fe
Petr Vorel <pvorel@suse.cz>
2026-07-09 15:55:58 +0200

    metadata: test.sh: Add support for building out-of-tree
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- metadata/tests/test.sh
+++ metadata/tests/test.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) Linux Test Project, 2021-2026
 
 fail=0
 
+BUILDDIR="${BUILDDIR:-.}"
+
+cd "${SRCDIR:-.}"
+
 for i in *.c; do
 	printf "* $i "
-	../metaparse $i > tmp.json
-	if ! diff tmp.json $i.json >/dev/null 2>&1; then
+	$BUILDDIR/../metaparse $i > $BUILDDIR/tmp.json
+	if ! diff $BUILDDIR/tmp.json $i.json >/dev/null 2>&1; then
 		echo '[FAIL]'
 		echo "$i output differs!"
-		diff -u tmp.json $i.json
+		diff -u $BUILDDIR/tmp.json $i.json
 		fail=1
 	else
 		echo '[OK]'
 	fi
 done
 
-rm -f tmp.json
+rm -f $BUILDDIR/tmp.json
 
 exit $fail
