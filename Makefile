pevik/ltp

commit 73245ef9922d63eee987f6aaea146a2727ee33f5

make: Add test-metadata target
And run it as part of make test.

Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

Makefile
@@ -207,6 +207,7 @@ ifneq ($(build),$(host))
 	$(error running tests on cross-compile build not supported)
 endif
 	$(call _test)
+	$(MAKE) test-metadata
 
 test-c: lib-all
 ifneq ($(build),$(host))
@@ -219,6 +220,9 @@ ifneq ($(build),$(host))
 	$(error running tests on cross-compile build not supported)
 endif
 	$(call _test,-s)
+
+test-metadata:
+	$(MAKE) -C $(abs_srcdir)/metadata/ test
 
 ## Help
 .PHONY: help

commit 8df47549de0b7dd440c482b46715ebdb23e997d2

Makefile: Skip test-metadata on cross compile
Fixes: 73245ef992 ("make: Add test-metadata target")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

Makefile
@@ -210,6 +210,9 @@ endif
 	$(top_srcdir)/testcases/lib/run_tests.sh -b $(abs_builddir)
 
 test-metadata: metadata-all
+ifneq ($(build),$(host))
+	$(error running tests on cross-compile build not supported)
+endif
 	$(MAKE) -C $(abs_srcdir)/metadata test
 
 MODULE_DIRS :=  $(shell \