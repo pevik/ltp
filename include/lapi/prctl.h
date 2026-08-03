737a629ebdaba4bf8af192f31a976775500b99c9
Andrea Cervesato <andrea.cervesato@suse.com>
2026-08-03 14:26:49 +0200

    lapi/prctl: add more fallback definitions
    Add the following fallback definitions:
    
    - PR_SET_MM
    - PR_SET_MM_MAP
    - PR_SET_MM_MAP_SIZE
    - struct prctl_mm_map
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- include/lapi/prctl.h
+++ include/lapi/prctl.h
@@ -7,6 +7,7 @@
 #ifndef LAPI_PRCTL_H__
 #define LAPI_PRCTL_H__
 
+#include <stdint.h>
 #include <sys/prctl.h>
 
 #ifndef PR_SET_NAME
@@ -57,6 +58,30 @@
 #ifndef PR_GET_SPECULATION_CTRL
 # define PR_GET_SPECULATION_CTRL 52
 # define PR_SET_SPECULATION_CTRL 53
+#endif
+
+#ifndef PR_SET_MM
+# define PR_SET_MM		35
+#endif
+#ifndef PR_SET_MM_MAP
+# define PR_SET_MM_MAP	14
+#endif
+#ifndef PR_SET_MM_MAP_SIZE
+# define PR_SET_MM_MAP_SIZE	15
+#endif
+
+#if !(HAVE_STRUCT_PRCTL_MM_MAP) && !defined(PR_SET_MM_MAP)
+struct prctl_mm_map {
+	uint64_t start_code, end_code;
+	uint64_t start_data, end_data;
+	uint64_t start_brk, brk;
+	uint64_t start_stack;
+	uint64_t arg_start, arg_end;
+	uint64_t env_start, env_end;
+	uint64_t *auxv;
+	uint32_t auxv_size;
+	uint32_t exe_fd;
+};
 #endif
 
 #endif /* LAPI_PRCTL_H__ */
