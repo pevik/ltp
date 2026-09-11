pevik/ltp

commit ef4c5c61f86e6d63103516f8d4356c7b0e1d762d

include: Fix kernel-doc warnings in header comments
Drop :ref: cross-references to undocumented structs in tst_test.h
and tst_kvercmp.h kernel-doc comments, replacing them with plain
text. These produced 'undefined label' Sphinx warnings.

Downgrade kernel-doc comments to plain comments for struct tst_cmd
and TST_CAP() macro to avoid name collisions with the tst_cmd()
function and struct tst_cap respectively. The kernel-doc parser
flattens names case-insensitively, causing 'name used several times'
errors and duplicate target warnings.

Fixes: b7df4320b0 ("tst_test.h: Link various structs in sphinx doc")
Fixes: 597613727a ("doc: Convert tst_kvercmp.h to RST")
Reviewed-by: Petr Vorel <pvorel@suse.cz>
Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

include/tst_capability.h
@@ -67,7 +67,7 @@ struct tst_cap {
 	char *name;
 };
 
-/**
+/*
  * TST_CAP() - Create a struct tst_cap entry.
  *
  * @action: What should we do, i.e. drop or add capability.

include/tst_cmd.h
@@ -19,7 +19,7 @@ enum tst_cmd_flags {
 	TST_CMD_TCONF_ON_MISSING = 2,
 };
 
-/**
+/*
  * struct tst_cmd - Provides details about a command struct needed by LTP test.
  * @cmd: The name of the command.
  * @optional: A flag indicating if the command is optional.

include/tst_kvercmp.h
@@ -93,7 +93,7 @@ struct tst_kern_exv {
  * @r1: Major kernel version.
  * @r2: Minor kernel version.
  * @r3: Kernel patch level.
- * @vers: A {} terminated array of :ref:`struct tst_kern_exv`.
+ * @vers: A {} terminated array of struct tst_kern_exv.
  *
  * Attempts to look up a distro specific kernel version from the struct
  * tst_kern_exv table first and if no match is found falls back to the version

include/tst_test.h
@@ -298,7 +298,7 @@ struct tst_fs {
  *
  * @tcnt: A number of tests. If set the test() callback is called tcnt times
  *        and each time passed an increasing counter value.
- * @options: An NULL optstr terminated array of :ref:`struct tst_option`.
+ * @options: An NULL optstr terminated array of struct tst_option.
  *
  * @min_kver: A minimal kernel version the test can run on. e.g. "3.10".
  *
@@ -322,7 +322,7 @@ struct tst_fs {
  * @forks_child: Has to be set if the test intends to fork children.
  *
  * @needs_device: If set a block device is prepared for the test, the device
- *                path and size are set in the :ref:`struct tst_device` variable
+ *                path and size are set in the struct tst_device variable
  *                called tst_device. If $LTP_DEV variable exists in the test
  *                environment the test attempts to use that device first and
  *                only if that fails the test falls back to use loop devices.
@@ -361,7 +361,7 @@ struct tst_fs {
  *                   i.e. file system that is supported by the kernel and has
  *                   mkfs installed on the system.The file system is mounted at
  *                   tst_test.mntpoint and file system details, e.g. type are set
- *                   in the :ref:`struct tst_device`. Each execution is independent,
+ *                   in the struct tst_device. Each execution is independent,
  *                   that means that for each iteration tst_test.setup() is
  *                   called at the test start and tst_test.cleanup() is called
  *                   at the end and tst_brk() only exits test for a single
@@ -500,7 +500,7 @@ struct tst_fs {
  *
  * @save_restore: A {} terminated array of /proc or /sys files that should
  *                saved at the start of the test and restored at the end. See
- *                tst_sys_conf_save() and :ref:`struct tst_path_val` for details.
+ *                tst_sys_conf_save() and struct tst_path_val for details.
  *
  * @ulimit: A {} terminated array of process limits RLIMIT_* to be adjusted for
  *          the test.
@@ -522,14 +522,14 @@ struct tst_fs {
  * @bufs: A description of guarded buffers to be allocated for the test. Guarded
  *        buffers are buffers with poisoned page allocated right before the start
  *        of the buffer and canary right after the end of the buffer. See
- *        :ref:`struct tst_buffers` and tst_buffers_alloc() for details.
+ *        struct tst_buffers and tst_buffers_alloc() for details.
  *
  * @caps: A {} terminated array of capabilities to change before the start of
- *        the test. See :ref:`struct tst_cap` and tst_cap_setup() for details.
+ *        the test. See struct tst_cap and tst_cap_setup() for details.
  *
- * @tags: A {} terminated array of test tags. See :ref:`struct tst_tag` for details.
+ * @tags: A {} terminated array of test tags. See struct tst_tag for details.
  *
- * @needs_cmds: A NULL terminated array of :ref:`struct tst_cmd` required for the test to run.
+ * @needs_cmds: A NULL terminated array of struct tst_cmd required for the test to run.
  *
  * @needs_cgroup_ver: If set the test will run only if the specified cgroup
  *                    version is present on the system.

commit 25cdd46b31bb65a6309469eda144a6414935b45a

include: Restore kerneldoc for tst_cmd() and TST_CAP()
This partly reverts ef4c5c61f8. We want to have the doc included,
because both the structure and the function is documented properly
there. We need to use c:func: and c:type: in references, but that's ok.

Fixes: ef4c5c61f8 ("include: Fix kernel-doc warnings in header comments")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/tst_capability.h
@@ -69,7 +69,7 @@ struct tst_cap {
 	char *name;
 };
 
-/*
+/**
  * TST_CAP() - Create a struct tst_cap entry.
  *
  * @action: What should we do, i.e. drop or add capability.

include/tst_cmd.h
@@ -19,7 +19,7 @@ enum tst_cmd_flags {
 	TST_CMD_TCONF_ON_MISSING = 2,
 };
 
-/*
+/**
  * struct tst_cmd - Provides details about a command struct needed by LTP test.
  * @cmd: The name of the command.
  * @optional: A flag indicating if the command is optional.