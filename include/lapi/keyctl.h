pevik/ltp

commit ee02118e9f9c8148a820c1747e03263890742abf

lapi/keyctl.h: Add fallback definitions for extended ops
Add fallback definitions for KEYCTL_LINK, KEYCTL_RESTRICT_KEYRING,
KEYCTL_MOVE, and KEYCTL_MOVE_EXCL, as well as fallback definitions
for struct keyctl_dh_params and struct keyctl_kdf_params when built
without linux/keyctl.h.

Link: https://lore.kernel.org/ltp/20260916140402.1797325-2-pvorel@suse.cz/
Reviewed-by: Petr Vorel <pvorel@suse.cz>
Tested-by: Petr Vorel <pvorel@suse.cz>
Tested-by: Li Wang <li.wang@linux.dev>
Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
[ pvorel
* Remove <keyutils.h> usage - use only <linux/keyctl.h>
* Add check for struct keyctl_dh_params.priv for Leap 42.2 ]
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/lapi/keyctl.h
@@ -8,6 +8,8 @@
 
 #include "config.h"
 
+#include <stdint.h>
+
 #if defined(HAVE_KEYUTILS_H) && defined(HAVE_LIBKEYUTILS)
 # include <keyutils.h>
 #else
@@ -16,7 +18,6 @@
 # endif /* HAVE_LINUX_KEYCTL_H */
 
 # include <stdarg.h>
-# include <stdint.h>
 # include "lapi/syscalls.h"
 typedef int32_t key_serial_t;
 
@@ -59,6 +60,23 @@ static inline key_serial_t keyctl_join_session_keyring(const char *name) {
 }
 
 #endif /* defined(HAVE_KEYUTILS_H) && defined(HAVE_LIBKEYUTILS) */
+
+#ifndef HAVE_STRUCT_KEYCTL_DH_PARAMS
+struct keyctl_dh_params {
+	int32_t priv;
+	int32_t prime;
+	int32_t base;
+};
+#endif
+
+#ifndef HAVE_STRUCT_KEYCTL_KDF_PARAMS
+struct keyctl_kdf_params {
+	char *hashname;
+	char *otherinfo;
+	uint32_t otherinfolen;
+	uint32_t __spare[8];
+};
+#endif
 
 /* special process keyring shortcut IDs */
 #ifndef KEY_SPEC_THREAD_KEYRING
@@ -124,6 +142,10 @@ static inline key_serial_t keyctl_join_session_keyring(const char *name) {
 # define KEYCTL_CLEAR 7
 #endif
 
+#ifndef KEYCTL_LINK
+# define KEYCTL_LINK 8
+#endif
+
 #ifndef KEYCTL_UNLINK
 # define KEYCTL_UNLINK 9
 #endif
@@ -166,6 +188,18 @@ static inline key_serial_t keyctl_join_session_keyring(const char *name) {
 
 #ifndef KEYCTL_WATCH_KEY
 # define KEYCTL_WATCH_KEY 32
+#endif
+
+#ifndef KEYCTL_RESTRICT_KEYRING
+# define KEYCTL_RESTRICT_KEYRING 29
+#endif
+
+#ifndef KEYCTL_MOVE
+# define KEYCTL_MOVE 30
+#endif
+
+#ifndef KEYCTL_MOVE_EXCL
+# define KEYCTL_MOVE_EXCL 0x00000001 /* do not displace from the to-keyring */
 #endif
 
 /* key permissions */

m4/ltp-keyutils.m4
@@ -1,10 +1,17 @@
 dnl SPDX-License-Identifier: GPL-2.0-or-later
 dnl Copyright (c) 2016 Fujitsu Ltd.
 dnl Copyright (c) 2017 Petr Vorel <pvorel@suse.cz>
+dnl Copyright (c) Linux Test Project, 2019-2026
 dnl Author: Xiao Yang <yangx.jy@cn.fujitsu.com>
 
 AC_DEFUN([LTP_CHECK_KEYUTILS_SUPPORT], [
 	AC_CHECK_LIB([keyutils], [add_key],
 	[AC_DEFINE(HAVE_LIBKEYUTILS, 1, [Define to 1 if you have libkeyutils installed.])
 	      AC_SUBST(KEYUTILS_LIBS, "-lkeyutils")])
+
+	AC_CHECK_TYPES([struct keyctl_dh_params, struct keyctl_kdf_params,
+			struct keyctl_pkey_query, struct keyctl_pkey_params],,,
+			[#include <linux/keyctl.h>])
+
+	AC_CHECK_MEMBERS([struct keyctl_dh_params.priv],,,[#include <linux/keyctl.h>])
 ])

commit bbdc9d95d4228bc28fdcab185db9304189752a21

keyctl: Vendor struct keyctl_dh_params
Copy struct keyctl_dh_params definition into LTP specific struct
ltp_keyctl_dh_params. (Use struct definition from keyutils.h from
keyutils userspace library, UAPI in kernel keeps also private hidden
from C++ by #ifndef guard which we 1) don't need 2) cannot use nested #ifndef.)

This helps to fix compilation on old kernel < 4.20:

    keyctl25.c:25:11: error: ‘struct keyctl_dh_params’ has no member named ‘priv’

'priv' struct member was added in kernel v4.20 (which we still support)
in commit 2ecefa0a15fd0 ("keys: Fix the use of the C++ keyword "private"
in uapi/linux/keyctl.h")

Vendoring struct helps us to remove autotools check (speedup).

Fixes: ee02118e9f ("lapi/keyctl.h: Add fallback definitions for extended ops")
Reported-by: linuxtestproject.agent@gmail.com
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/lapi/keyctl.h
@@ -61,13 +61,11 @@ static inline key_serial_t keyctl_join_session_keyring(const char *name) {
 
 #endif /* defined(HAVE_KEYUTILS_H) && defined(HAVE_LIBKEYUTILS) */
 
-#ifndef HAVE_STRUCT_KEYCTL_DH_PARAMS
-struct keyctl_dh_params {
+struct ltp_keyctl_dh_params {
 	int32_t priv;
 	int32_t prime;
 	int32_t base;
 };
-#endif
 
 #ifndef HAVE_STRUCT_KEYCTL_KDF_PARAMS
 struct keyctl_kdf_params {

m4/ltp-keyutils.m4
@@ -9,9 +9,6 @@ AC_DEFUN([LTP_CHECK_KEYUTILS_SUPPORT], [
 	[AC_DEFINE(HAVE_LIBKEYUTILS, 1, [Define to 1 if you have libkeyutils installed.])
 	      AC_SUBST(KEYUTILS_LIBS, "-lkeyutils")])
 
-	AC_CHECK_TYPES([struct keyctl_dh_params, struct keyctl_kdf_params,
-			struct keyctl_pkey_query, struct keyctl_pkey_params],,,
+	AC_CHECK_TYPES([struct keyctl_kdf_params, struct keyctl_pkey_query, struct keyctl_pkey_params],,,
 			[#include <linux/keyctl.h>])
-
-	AC_CHECK_MEMBERS([struct keyctl_dh_params.priv],,,[#include <linux/keyctl.h>])
 ])

testcases/kernel/syscalls/keyctl/keyctl25.c
@@ -15,7 +15,7 @@
 #include "keyctl_common.h"
 #include "keyctl_dh_data.h"
 
-static struct keyctl_dh_params *dh_params;
+static struct ltp_keyctl_dh_params *dh_params;
 static unsigned char out_buf[256];
 
 static void setup(void)

testcases/kernel/syscalls/keyctl/keyctl26.c
@@ -15,7 +15,7 @@
 #include "keyctl_common.h"
 #include "keyctl_dh_data.h"
 
-static struct keyctl_dh_params *dh_params;
+static struct ltp_keyctl_dh_params *dh_params;
 
 static void setup(void)
 {

testcases/kernel/syscalls/keyctl/keyctl27.c
@@ -19,7 +19,7 @@
 
 #define KDF_OTHERINFO	"LTP-KDF-TEST-INFO"
 
-static struct keyctl_dh_params *dh_params;
+static struct ltp_keyctl_dh_params *dh_params;
 static struct keyctl_kdf_params *kdf_params;
 static unsigned char out_buf[32];
 

testcases/kernel/syscalls/keyctl/keyctl28.c
@@ -21,9 +21,9 @@
 #include "keyctl_common.h"
 #include "keyctl_dh_data.h"
 
-static struct keyctl_dh_params *dh_params;
-static struct keyctl_dh_params *dh_bogus_prime;
-static struct keyctl_dh_params *dh_nonuser_prime;
+static struct ltp_keyctl_dh_params *dh_params;
+static struct ltp_keyctl_dh_params *dh_bogus_prime;
+static struct ltp_keyctl_dh_params *dh_nonuser_prime;
 
 static struct keyctl_kdf_params *kdf_valid;
 static struct keyctl_kdf_params *kdf_spare_nonzero;
@@ -33,7 +33,7 @@ static struct keyctl_kdf_params *kdf_unknown_hash;
 static unsigned char out_buf[1025];
 
 static struct tcase {
-	struct keyctl_dh_params **params;
+	struct ltp_keyctl_dh_params **params;
 	void *buffer;
 	size_t buflen;
 	struct keyctl_kdf_params **kdf;
@@ -105,7 +105,7 @@ static void setup(void)
 static void verify_negative(unsigned int n)
 {
 	struct tcase *tc = &tcases[n];
-	struct keyctl_dh_params *p = *tc->params;
+	struct ltp_keyctl_dh_params *p = *tc->params;
 	struct keyctl_kdf_params *kdf = tc->kdf ? *tc->kdf : NULL;
 
 	TST_EXP_FAIL2(keyctl(KEYCTL_DH_COMPUTE, (unsigned long)p,